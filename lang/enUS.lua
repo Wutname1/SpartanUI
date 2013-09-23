fb0161292d88c72297a1d035cace8b91ebe190d8
wutname1 <wutname1@75834508-ef64-48bf-aeaf-83d1281c7be1>
2013-09-23 04:30:36 +0000

    Convert to Automated Localization
    git-svn-id: svn://svn.curseforge.net/wow/spartan-ui/mainline/trunk@68 75834508-ef64-48bf-aeaf-83d1281c7be1

--- lang/enUS.lua
+++ lang/enUS.lua
@@ -1,17 +1,3 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("SpartanUI", "enUS", true)
 
-------------------
--- Global Features
-
---Options setup
-L["Main"] = "Main"
-L["General"] = "General"
-L["PlayerFrames"] = "Player Frames"
-L["PartyFrames"] = "Party Frames"
-L["RaidFrames"] = "Raid Frames"
-L["SpinFrames"] = "Spin Cam"
-L["FilmFrames"] = "Film Effects"
-
-L["Version"] = "Version"
-L["ResetDatabase"] = "Reset Database"
-L["ResetDatabaseDesc"] = "Will Reset the ENTIRE Database. This should fix 99% of Settings related issues."
+@localization(locale="enUS", format="lua_table")@
