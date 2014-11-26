a20f7e659ebfce98e81fdd0f1047b2938cd5f128
wutname1 <wutname1@75834508-ef64-48bf-aeaf-83d1281c7be1>
2014-11-26 23:18:47 +0000

    lang fix?
    git-svn-id: svn://svn.curseforge.net/wow/spartan-ui/mainline/trunk@126 75834508-ef64-48bf-aeaf-83d1281c7be1

---
+++ lang/esES.lua
@@ -0,0 +1,3 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("SpartanUI", "esES")
+
+--@localization(locale="esES", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
