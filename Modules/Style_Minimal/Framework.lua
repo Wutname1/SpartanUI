8306cb69e26b1c4432300c7afe4d6d1bd69b912a
wutname1 <wutname1@75834508-ef64-48bf-aeaf-83d1281c7be1>
2015-07-18 21:17:46 +0000

    fix corrupted files
    git-svn-id: svn://svn.curseforge.net/wow/spartan-ui/mainline/trunk@189 75834508-ef64-48bf-aeaf-83d1281c7be1

--- Modules/Style_Minimal/Framework.lua
+++ Modules/Style_Minimal/Framework.lua
@@ -194,14 +194,114 @@ function module:InitFramework()
 end
 
 function module:SetupVehicleUI()
-	if DBMod.Artwork.VehicleUI thenö´#éâ°çÆÃÇdét¢P   [o9ÑÌ—õ÷ß İÆŒ¼Bu	   ÷
-Ã¤áe43†ÆĞ‹?ÆüÂ   +9ˆ‚„~¤mı6¾Aä›­½_   ÑBï•İöÑînôñõ×µyèõ   -Rø¶M˜æùlBÜã4ç   Ø^»iUÿ±¤$RKâu®   Œ?Ï×=º#yâoçâ}ú    d¬¶³s¥#ô5P÷áe„   ZŞ«Ú}CÔIÀ³§Túæ€
-  %Ç@’¹­\™ÜŸpö=  ‘éµ´q¡›°Ø„yŸyÉ×   QkpÉÈ$@JA{j ¢p³p   êKLíâÜd¥9 ®Ù§fL   )ç¹ç¼­*Úò¹.‡¤ °–'ad   Y?°¢Äe«"v— Ğ|Œ^‚  Â¢q¨Å«lÆëËÖ¡‡7×±   ^İ¦¹q°’ŠŒÿ¤q¡?)Š)   seİªÍˆë«Ì¡šrŠ8  "Â‹ÇeuìÁQjïÜ¡¢}2š   =°²¡¥]ÔÉs7«·¢"Š;`          ÉõÔàÙï÷eĞnÃ£`ù9À  ‚ù¾iš7ëWŠí¤˜-G{  Ì…»I”R;A€¤µ
-øÑ   ğPG‡„‰Ä“n ¹¤ºcøñ    c\`ég½s{êÂ-¤àÁ)/­   =âË‰mğˆŒ0Ãgš‹¤ä®©âß  AQœ‰iŒk®Ã~¤ë´Qî   nÖ®Yá©)à´¶a¤óNÿ…Z   ÕŞ“ÍÇeÙ6à/éô¥z4ß  (¿}˜µ¿_¤Gk]Ì¥ş:Ú    ­ZéòfÛÇZ÷ß¥¨·i   ‰oªó¡lé‹úV¥•,’=Ñ   IaSëá*ŠmÆĞ&;¥ÅNm   ¾(ƒÒ`1²İÂ›1w¥Æ¼›m   5ë­ÄÏ—=D­¥ó/½/   /=OœëîÏƒ_´ş4Ş¦0«8ï  ÀPí+©E ‹Ê¦7Tú   º –Õô]5Ö	Î	¦K–L$   /­ÏG[ú­(?'`¦k S#:   _œ‰¤"Ï¨ŸÇ„’¦¯X¯òú  TŒ	¤ø|DA0epb¦ËÁUg4           ‘6bêRV,C+“8Ù§5¦Ï  è‚¹v1Ó¬—÷í§>tœ¯  ¼®MKSÓ„ûZ
-§¨,Zúb  ZØ5Ñœİræ¤0§´×òœ   =Ãk±ºo[1-GQİ§Õvî‡;  ÒhÙ—ïækPzıÃ¨&™K%š  ewOÍ%’¾öÒŒ„¨1^>f   ŠØê¾Ñ÷©EBŠäµ¨Z‚¦ğ   šç6Ê6ß—SüWî¨d<+¼U   ³É—zÄ1Ç£_xt¨o¶/   i?D¢ı×ÔÖ<¨oy¶'   	à¬ÿ˜İLÜ…Á›¨cæiı   mÙÏÄpÊéUü‘ê•¨©óBQ   B7öÙw@ß,¢¬Ùë×¨²^<a7    ?¢MˆwÛ¨²•(T   ù<€ÔË„mí}¨¿Ì¤Ş%   ¾iŞ¥c·G—@e0a¨ÁR@à5   #„/ÎìÂÒïÅé¨Å™kªA   çô>T£…ºë{a¨Ê9È   †İ}Á1şI'Ì‚«A¨××Í9   ]òãó1ó/g#ÜH‹©&”g=F           Jh¨µIÃW Í¶Ç[©-Ol   ?óúñÓwT(2PEö©-tcp)   BªÑçÀåŞHØRr©.1ÛV1   VR/Ÿµà•wLU4Ú¿Õ)"Îc  1ı¶x¬Ök¨ñ¿Ü¹²*  C[ÿÓzİøĞ¤üR³¿óª)ÿ¸   z€¡ÆŒé+ºU¿÷
-ûw  Y«7ævÖešÇmÌX¿úbŒrÎ   vÊchNkåå°¿û0şA„   ‹ÕĞ¼² ¤…Ø¨bÃ¾šCË  ²”£÷çGãgf}×tÔiğF   ê	é~ì¥^o›XÆ[Ôk—KT   Â‡A®®c?XRvEãøÔ}_°È6  ¯7ÊÑLÀÑôåCÔ€_Äy   ÆZûº.‚Œ­s²$¦ÔƒÄœSM   ¥ƒYÅªI0Ùµ×§?Ô‹â%ïn   ~ŒÒX
-ÈÚ«´EWÔÑŸí>   ™Ü¬ãa$.Ç¿Ÿ8òiÕ`í  j…«2k%Û'áÕ:i‹ˆ   :3„ıyÈ°¦¬6ÕW4ù|A   Ê“´‘Wİ+ú.HVpÕbÍi¿,           tJ°86qÅügÕek3   \	ô¾\`\ùÆ@KÕh!Úm   'û›ûg‰gşŞÛ7Õi…t   áSm¯.À³Üé_ñÕjİßC   Rò“lóˆ7ÜÔŒÔÕo¡2š     jƒÛ3ãe¤“vÕpÜ$   PÔîÉ’8˜#ÎŒv-ÕTïŠF   q¸¿ vq{Â`«ÜÓÕ‘bâp7   ï{ş¾`ScêğeL·Õ‘ìÔ>   Ú¼ZÁª¯»÷$¡|æ,Õ’JíŒ   ­×ô™YGy*ÃCwy¨ÕšDŠL   yA•àˆ‰i‰FoÕThK{   «¦iÁÀ†…½jˆÕ¥Å¢   °ğıãş7~pÚŞÕ¦ !   9×h‹—›Q÷ILõåÕ¦ÚvæU   Ì’ƒ«Te3ó¨¤Õ»{C8,   ˆ‹›Âæ9œQŒ@cÕ¾I5   >¨±œÕN‘Ñh7ÕŞË±ş  â­ƒœØˆn§Ö,Ë(ĞÅ  ‡W—+B™ÒjäŠ=Ö‚Å²5:   N y¼úğE•`Æ	8ÖÜrP3ÿ          şÀ4·Úóí¾©ªm*ª×/F€¨,   Æ]·„õÆGKóAÒ×RA_0   Š3˜SØs^Š:)øŸ×R’€0   ë´ÁÃ0CşáÂWa]ê×WäÉL0   î®Ù¸™2QZXL8¹×v ^Çë  p‡Ò½ØğŒ}Ó‡×ºı^ŞO   ¬æç®ãÂ™vB„;l×¿@[‰Ü   óOröºoÍ»¦éğ8|Ø&^!   Î¹­‘~f@+;R™Ø*JzMd   áëõ´’ø>xÇ.Ø,(bÜ(   ƒXÁy¸(\Ä–×Ø.bZ  µ&ù›Lmñhj¤ØE´à:   [çFÖ€^W†ØUûN·}  ß•ò¯t¬yš§uØ×úØe˜ ¼   =€4ÀÂ¥œğ!w«ØfU/#   ÑãZ–œµmŞ$¸Øï-Øˆ²ˆÁl   §Åô¯Vgb*½¡¯[Øÿ×0{Ï  Ó…¤¦-çÇ*Æ£ÙvÙàDå˜W  šÚ©jfÔ'h¿ÓcÙúto~  MS8Ï»Üµ§#N†ÚŞšœ   r fƒ…•Ç03î#ÚuF_Ø            2ÏÅ“7X·ÖD:Ú‚ÿc   YdH»Ù/§!ƒd¤QMÚ„SæeJ   èÓ£ƒçV\QlÚ„K˜I   ½KäØ\¿oV“-e³Ú‹˜÷‘H   5PŒ`¯y¥g*$ÚmK l   |­š
-—ØlÊÏèÚ“œ®06   Ì´ÄÕ	 pÀtÉÚ”)za%   IYD€+÷İ&«î§ë2Ú”s.   Wòõ»E‚×7´˜¯­sÚ•Ÿö$   S"S÷~©»ôH›ãÏÀÚ˜¾   §IV™u=¤Ã=Q–Ú›K‘/   itL§b°I*ÁA}±ğÚ£ gæ%   ?ü)QD•´$a%CÚ¦¼“±ê   ×Z—Â&6üİ}Ú«:
-Y   Æ #ÌÇ)øíáª~Â#Ú´Ad§w   B†»Í÷nm—Ü<7íÚµ×³±5   K€+¼£¢_Ã‚à
-%3Ú·´X¬c   «[J½áL¸œO.Y@üÚ¸Ÿ{#   Ö8ŞúÉ¼%§Æ³Ø¤Ú»4ö5   5r‹…mÙ>5,uAÚÔ25 W   ”½Ò5ª¿$İ˜`Œ×ÚÕÅ8ğš           ˆîú½Ô	Ö’­ñ¬ÚñÊ£E	   ˆİ­è¿/æ-h‹›b*Úıñtë:   ø«q±¢Dí¡
-;:(ÛK‰·-   0¯Ô„··[yKQgÛ$Ä`…w   ²‚ÿ˜tº ú°ÔökÛ&½+Ä<   ·ìÍ¶jø”ÒÄAxƒÛuı¨B   NóA„0>™;JÑ?ºêÛy/   °.æ¹ƒ  ohPíÛ{:1°g   ¦ï9¬ ‡{/aÎŞ¦Û’*—ZE   ŠÓºU¥[yKzÂ}Û•¡#F   8±‡W%}y/2à4ÛšvF@   ®äƒï¶®;;ÍÍ=åÛœj8À   Ê»ÿs¬Íô[ƒ¯ÛœÜ¢e   İ¬¢—›;Q:ÎØ6„Û¡§åM   fñÎşÈÓ
+	if DBMod.Artwork.VehicleUI then
+		RegisterStateDriver(Minimal_SpartanUI, "visibility", "[petbattle][overridebar][vehicleui] hide; show");
+	end
+end
+function module:RemoveVehicleUI()
+	if DBMod.Artwork.VehicleUI then
+		UnRegisterStateDriver(Minimal_SpartanUI, "visibility");
+	end
+end
+
+function module:EnableFramework()
+	do -- create the tooltip
+		tooltip = CreateFrame("Frame","Minimal_StatusBarTooltip",Minimal_SpartanUI,"Minimal_StatusBars_TooltipTemplate");
+		Minimal_StatusBarTooltipHeader:SetJustifyH("LEFT");
+		Minimal_StatusBarTooltipText:SetJustifyH("LEFT");
+		Minimal_StatusBarTooltipText:SetJustifyV("TOP");
+		spartan:FormatFont(Minimal_StatusBarTooltipHeader, 12, "Core")
+		spartan:FormatFont(Minimal_StatusBarTooltipText, 10, "Core")
+	end
+	
+	module:SetColor()
+	
+	anchor:SetFrameStrata("BACKGROUND"); anchor:SetFrameLevel(1);
+	frame:SetFrameStrata("BACKGROUND"); frame:SetFrameLevel(1);
+	
+	hooksecurefunc("AchievementAlertFrame_ShowAlert",function() -- achivement alerts
+		if (AchievementAlertFrame1) then AchievementAlertFrame1:SetPoint("BOTTOM",Minimal_SpartanUI,"TOP",0,100); end
+	end);
+	hooksecurefunc("UIParent_ManageFramePositions",function()
+		TutorialFrameAlertButton:SetParent(Minimap);
+		TutorialFrameAlertButton:ClearAllPoints();
+		TutorialFrameAlertButton:SetPoint("CENTER",Minimap,"TOP",-2,30);
+		CastingBarFrame:ClearAllPoints();
+		CastingBarFrame:SetPoint("BOTTOM",frame,"TOP",0,90);
+	end);
+	
+	module:SetupVehicleUI();
+
+	module:updateScale();
+	module:updateOffset();
+	module:updateXOffset();
+	module:updateViewport();
+	
+	-- Limit updates via interval
+	anchor.UpdateInterval = 5 --Seconds
+	anchor.TimeSinceLastUpdate = 0
+	anchor:SetScript("OnUpdate",function(self,...)
+		local elapsed = select(1,...)
+		self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 
+		if (self.TimeSinceLastUpdate > self.UpdateInterval) then
+			if (InCombatLockdown()) then return; end
+			
+			module:updateScale();
+			module:updateOffset();
+			module:updateXOffset();
+			module:updateViewport();
+			self.TimeSinceLastUpdate = 0
+			
+			if DB.OpenOptions then
+				spartan:ChatCommand()
+				DB.OpenOptions = false;
+			end
+		end
+	end);
+	
+
+	do
+		function My_VehicleSeatIndicatorButton_OnClick(self, button)
+			local seatIndex = self.virtualID;
+			local controlType, occupantName = UnitVehicleSeatInfo("player", seatIndex);
+			if ( button == "RightButton" and ( CanEjectPassengerFromSeat(seatIndex) or (CanExitVehicle() and ( occupantName == UnitName("player") )))) then
+				ToggleDropDownMenu(1, seatIndex, VehicleSeatIndicatorDropDown, self:GetName(), 0, -5);
+				if ( CanEjectPassengerFromSeat(seatIndex) ) then
+					UIDropDownMenu_DisableButton(1,2);
+					UIDropDownMenu_EnableButton(1,1);
+				else
+					UIDropDownMenu_DisableButton(1,1);
+					UIDropDownMenu_EnableButton(1,2);
+				end
+			else
+				UnitSwitchToVehicleSeat("player", seatIndex);
+			end
+		end
+		
+		local old_VehicleSeatIndicatorButton_OnClick = VehicleSeatIndicatorButton_OnClick
+		VehicleSeatIndicatorButton_OnClick = My_VehicleSeatIndicatorButton_OnClick
+		
+		function My_VehicleSeatIndicatorDropDown_Initialize()
+			local info = UIDropDownMenu_CreateInfo();
+			info.text = EJECT_PASSENGER;
+			info.disabled = nil;
+			info.func = VehicleSeatIndicatorDropDown_OnClick;
+			UIDropDownMenu_AddButton(info);
+			info.text = VEHICLE_LEAVE;
+			info.disabled = nil;
+			info.func = VehicleSeatLeaveVehicleDropDown_OnClick;
+			UIDropDownMenu_AddButton(info);
+		end
+		
+		function VehicleSeatLeaveVehicleDropDown_OnClick()
+			VehicleExit();
+			PlaySound("UChatScrollButton");
+		end
+		
+		local old_VehicleSeatIndicatorDropDown_Initialize = VehicleSeatIndicatorDropDown_Initialize()
+		VehicleSeatIndicatorDropDown_Initialize = My_VehicleSeatIndicatorDropDown_Initialize
+		
+		UIDropDownMenu_Initialize( VehicleSeatIndicatorDropDown, VehicleSeatIndicatorDropDown_Initialize, "MENU");
+	end
+end
+
