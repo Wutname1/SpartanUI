96e158862059ed71bbf84136a7d8248f8e44d772
Jeremy <jeremy12@gmail.com>
2016-12-21 21:21:43 -0600

    Localization Test

--- lang/enUS.lua
+++ lang/enUS.lua
@@ -1,3 +1,7 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("SpartanUI", "enUS", true, debug)
 
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Frames", handle-subnamespaces="concat")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Film", handle-subnamespaces="concat")@
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="Spin", handle-subnamespaces="concat")@
