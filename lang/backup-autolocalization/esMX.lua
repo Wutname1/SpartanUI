bcc40eaf04199b684932bf313cd0f63981db7022
Jeremy <jeremy12@gmail.com>
2018-06-21 23:21:00 -0500

    Localization Update

--- lang/backup-autolocalization/esMX.lua
+++
@@ -1,4 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("SpartanUI", "esMX")
-if not L then return end
-
---@localization(locale="esMX", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
--- lang/backup-autolocalization/frFR.lua
+++
@@ -1,4 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("SpartanUI", "frFR")
-if not L then return end
-
---@localization(locale="frFR", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
