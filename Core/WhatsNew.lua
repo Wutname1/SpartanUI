099eb31bc34784225988dde24bc359c2dd4e26b5
Jeremy <jeremy12@gmail.com>
2018-07-28 12:39:17 -0500

    Removed First artifact notification.

--- Core/WhatsNew.lua
+++ Core/WhatsNew.lua
@@ -1,6 +1,5 @@
 local SUI = SUI
 local module = SUI:NewModule('WhatsNew')
-local ArtifactWatcher, loginlevel
 
 function SUI:WhatsNew()
 	local PageData = {
@@ -88,50 +87,6 @@ function SUI:WhatsNew()
 	SetupWindow:DisplayPage()
 end
 
-function module:FirstArtifact()
-	local PageData = {
-		title = L['Congratulations'],
-		SubTitle = L['You have equiped your first artifact weapon'],
-		Desc1 = L[
-			'Your artifact weapon levels up just like your character through a resource called Artifact Power. Would you like to track your artifact power as a status bar?'
-		] .. '|r',
-		Desc2 = L[
-			'This will replace in the bottom right status bar and you can switch between the two at any time via the SpartanUI settings.'
-		],
-		Display = function()
-			-- Track have been displayed
-			SUI.DBG.HasEquipedArtifact = true
-
-			--Container
-			SUI_Win.WhatsNew = CreateFrame('Frame', nil)
-			SUI_Win.WhatsNew:SetParent(SUI_Win.content)
-			SUI_Win.WhatsNew:SetAllPoints(SUI_Win.content)
-
-			-- Buttons
-			SUI_Win:SetSize(550, 170)
-			SUI_Win:ClearAllPoints()
-			SUI_Win:SetPoint('TOP', UIParent, 'TOP', 0, -50)
-			SUI_Win.Status:Hide()
-			SUI_Win.Skip:SetSize(130, 25)
-			SUI_Win.Skip:SetPoint('BOTTOMRIGHT', SUI_Win, 'BOTTOM', -10, 5)
-			SUI_Win.Skip:SetText('LEAVE IT AS IS')
-			SUI_Win.Next:SetSize(130, 25)
-			SUI_Win.Next:SetPoint('BOTTOMLEFT', SUI_Win, 'BOTTOM', 10, 5)
-			SUI_Win.Next:SetText('TRACK ARTIFACT POWER')
-		end,
-		Next = function()
-			SUI.DBMod.StatusBars[2].display = 'ap'
-			SUI:GetModule('Style_' .. SUI.DBMod.Artwork.Style):UpdateStatusBars()
-		end,
-		Skip = function()
-		end
-	}
-	local SetupWindow = SUI:GetModule('SetupWindow')
-
-	SetupWindow:AddPage(PageData)
-	SetupWindow:DisplayPage()
-end
-
 function module:OnInitialize()
 	if SUI.DBG.WhatsNew == nil then
 		SUI.DBG.WhatsNew = true
@@ -148,73 +103,4 @@ function module:OnInitialize()
 	-- Update SUI.DB Version
 	SUI.DB.Version = SUI.Version
 	SUI.DBG.Version = SUI.Version
-end
-
-function module:FirstAtrifactNotice()
-	--Only process if we are below 110; allowed to show new features; have never used an artifact; The style allows tracking
-	local TrackingAP = false
-	for _, v in ipairs(SUI.DBMod.StatusBars) do
-		if v.display == 'ap' then
-			TrackingAP = true
-		end
-	end
-
-	if
-		loginlevel < 110 and SUI.DBG.WhatsNew and not SUI.DBG.HasEquipedArtifact and
-			not C_ArtifactUI.IsEquippedArtifactMaxed() and
-			SUI.DB.Styles[SUI.DBMod.Artwork.Style].StatusBars.AP and
-			not TrackingAP
-	 then
-		--Detect if user already has a artifact
-		if HasArtifactEquipped() then
-			SUI.DBG.HasEquipedArtifact = true
-			return
-		end
-
-		--Create Watcher
-		ArtifactWatcher = CreateFrame('Frame')
-		ArtifactWatcher:SetScript(
-			'OnEvent',
-			function(self, event)
-				-- Add the hooks for inventory if player logged in sub 100
-				if loginlevel < 100 and UnitLevel('player') >= 100 then
-					if UnitLevel('player') >= 100 then
-						ArtifactWatcher = CreateFrame('Frame')
-						ArtifactWatcher:RegisterEvent('ARTIFACT_XP_UPDATE')
-						ArtifactWatcher:RegisterEvent('UNIT_INVENTORY_CHANGED')
-					end
-				end
-				if
-					HasArtifactEquipped() and not (SUI.DBMod.StatusBars[1].display == 'ap' or SUI.DBMod.StatusBars[2].display == 'ap')
-				 then
-					module:FirstArtifact()
-					ArtifactWatcher:UnregisterEvent('ARTIFACT_XP_UPDATE')
-					ArtifactWatcher:UnregisterEvent('UNIT_INVENTORY_CHANGED')
-					ArtifactWatcher:UnregisterEvent('PLAYER_LEVEL_UP')
-					ArtifactWatcher = nil
-				elseif (SUI.DBMod.StatusBars[2].display == 'ap' or SUI.DBMod.StatusBars[2].display == 'ap') then
-					ArtifactWatcher:UnregisterEvent('ARTIFACT_XP_UPDATE')
-					ArtifactWatcher:UnregisterEvent('UNIT_INVENTORY_CHANGED')
-					ArtifactWatcher:UnregisterEvent('PLAYER_LEVEL_UP')
-				end
-			end
-		)
-
-		--Setup update events
-		if UnitLevel('player') >= 100 then
-			ArtifactWatcher:RegisterEvent('ARTIFACT_XP_UPDATE')
-			ArtifactWatcher:RegisterEvent('UNIT_INVENTORY_CHANGED')
-		end
-		ArtifactWatcher:RegisterEvent('PLAYER_LEVEL_UP')
-	end
-end
-
-function module:FirstAzeriteItem()
-end
-
-function module:OnEnable()
-	loginlevel = UnitLevel('player')
-
-	module:FirstAtrifactNotice()
-	module:FirstAzeriteItem()
 end
