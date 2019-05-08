9ee2ab7a3161bcf2d98d3757b2a9cb74e5eebd39
wutname1 <jeremy12@gmail.com>
2019-05-07 20:58:04 -0500

    Inital setup page added

--- Components/InterruptAnnounce.lua
+++ Components/InterruptAnnounce.lua
@@ -228,3 +228,112 @@ function module:Options()
 		}
 	}
 end
+
+
+function module:FirstLaunch()
+	local PageData = {
+		ID = 'InterruptAnnouncer',
+		Name = L['Interrupt announcer'],
+		SubTitle = L['Interrupt announcer'],
+		-- Desc1 = L['Automatically turn on combat logging when entering a zone.'],
+		RequireDisplay = SUI.DB.InterruptAnnouncer.FirstLaunch,
+		Display = function()
+			local window = SUI:GetModule('SetupWizard').window
+			local SUI_Win = window.content
+			local StdUi = window.StdUi
+			if not SUI.DB.EnabledComponents.InterruptAnnouncer then
+				window.Skip:Click()
+			end
+
+			--Container
+			local cLog = CreateFrame('Frame', nil)
+			cLog:SetParent(SUI_Win)
+			cLog:SetAllPoints(SUI_Win)
+
+			-- Setup checkboxes
+			cLog.options = {}
+			cLog.options.alwayson = StdUi:Checkbox(cLog, L['Always on'], nil, 20)
+
+			cLog.options.inBG = StdUi:Checkbox(cLog, L['Announce logging in chat'], nil, 20)
+			cLog.options.inRaid = StdUi:Checkbox(cLog, L['Mythic'], 150, 20)
+			cLog.options.inParty = StdUi:Checkbox(cLog, L['Heroic'], 150, 20)
+			cLog.options.inArena = StdUi:Checkbox(cLog, L['Normal'], 150, 20)
+			cLog.options.outdoors = StdUi:Checkbox(cLog, L['Looking for raid'], 150, 20)
+
+			local items = {
+				{text = 'Instance chat', value = 'INSTANCE_CHAT'},
+				{text = 'RAID', value = 'RAID'},
+				{text = 'PARTY', value = 'PARTY'},
+				{text = 'SAY', value = 'SAY'},
+				{text = 'SMART', value = 'SMART'},
+				{text = 'No chat', value = 'SELF'}
+			}
+
+			cLog.options.announceLocation = StdUi:Dropdown(cLog, 190, 20, items, SUI.DB.EnabledComponents.InterruptAnnouncer.announceLocation)
+
+			-- Create Labels
+			cLog.modEnabled = StdUi:Checkbox(cLog, L['Module enabled'], nil, 20)
+			cLog.lblRaid = StdUi:Label(cLog, L['Raid settings'], 13)
+			cLog.lblDungeon = StdUi:Label(cLog, L['Dungeon settings'], 13)
+
+			-- Positioning
+			StdUi:GlueTop(cLog.modEnabled, SUI_Win, 0, -10)
+			StdUi:GlueBelow(cLog.options.alwayson, cLog.modEnabled, -100, -5)
+			StdUi:GlueRight(cLog.options.announce, cLog.options.alwayson, 5, 0)
+
+			-- Raid Settings
+			StdUi:GlueTop(cLog.lblRaid, cLog.modEnabled, -150, -80)
+			StdUi:GlueBelow(cLog.options.raidmythic, cLog.lblRaid, 0, -5)
+			StdUi:GlueRight(cLog.options.raidheroic, cLog.options.raidmythic, 5, 0)
+			StdUi:GlueRight(cLog.options.raidnormal, cLog.options.raidheroic, 5, 0)
+
+			StdUi:GlueBelow(cLog.options.raidlfr, cLog.options.raidmythic, 0, -5)
+			StdUi:GlueRight(cLog.options.raidlegacy, cLog.options.raidlfr, 5, 0)
+
+			--Dungeon Settings
+			StdUi:GlueBelow(cLog.lblDungeon, cLog.options.raidlfr, 0, -20)
+			StdUi:GlueBelow(cLog.options.mythicplus, cLog.lblDungeon, 0, -5)
+			StdUi:GlueRight(cLog.options.mythicdungeon, cLog.options.mythicplus, 5, 0)
+			StdUi:GlueRight(cLog.options.heroicdungeon, cLog.options.mythicdungeon, 5, 0)
+
+			StdUi:GlueBelow(cLog.options.normaldungeon, cLog.options.mythicplus, 0, -5)
+
+			-- Defaults
+			cLog.modEnabled:SetChecked(SUI.DB.EnabledComponents.InterruptAnnouncer)
+			for key, object in pairs(cLog.options) do
+				object:SetChecked(SUI.DB.InterruptAnnouncer[key])
+			end
+
+			cLog.modEnabled:HookScript(
+				'OnClick',
+				function()
+					for _, object in pairs(cLog.options) do
+						if cLog.modEnabled:GetChecked() then
+							object:Enable()
+						else
+							object:Disable()
+						end
+					end
+				end
+			)
+
+			SUI_Win.cLog = cLog
+		end,
+		Next = function()
+			local window = SUI:GetModule('SetupWizard').window
+			local cLog = window.content.cLog
+			SUI.DB.EnabledComponents.InterruptAnnouncer = cLog.modEnabled:GetChecked()
+
+			for key, object in pairs(cLog.options) do
+				SUI.DB.InterruptAnnouncer[key] = object:GetChecked()
+			end
+
+			SUI.DB.InterruptAnnouncer.FirstLaunch = false
+		end,
+		Skip = function()
+			SUI.DB.InterruptAnnouncer.FirstLaunch = false
+		end
+	}
+	local SetupWindow = SUI:GetModule('SetupWizard')
+	SetupWindow:AddPage(PageData)
+end
