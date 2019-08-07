129666187422a5eb4c46929c4e902c3687bef668
wutname1 <jeremy12@gmail.com>
2019-08-07 03:01:00 -0500

    Inital working version of Taunt watcher.

--- Components/TauntWatcher.lua
+++ Components/TauntWatcher.lua
@@ -1,59 +1,143 @@
 local SUI = SUI
-local module = SUI:NewModule('Component_TauntWatcher')
+local module = SUI:NewModule('Component_TauntWatcher', 'AceEvent-3.0')
 local L = SUI.L
 module.DisplayName = 'Taunt watcher'
 ----------------------------------------------------------------------------------------------------
+local TauntsList = {
+	--Warrior
+	355, --Taunt
+	--Death Knight
+	51399, --Death Grip for Blood (49576 is now just the pull effect)
+	56222, --Dark Command
+	--Paladin
+	62124, --Hand of Reckoning
+	--Druid
+	6795, --Growl
+	--Hunter
+	20736, --Distracting Shot
+	--Monk
+	115546, --Provoke
+	--Demon Hunter
+	185245, --Torment
+	--Paladin
+	204079 --Final Stand
+}
+local lastTimeStamp, lastSpellID = 0, 0
 
+local function printFormattedString(who, target, sid, failed)
+	local msg = SUI.DB.TauntWatcher.text
+	local ChatChannel = SUI.DB.TauntWatcher.announceLocation
 
-function module:OnInitialize()
-	local Defaults = {
-		active = {
-			always = false,
-			inBG = false,
-			inRaid = true,
-			inParty = true,
-			inArena = true,
-			outdoors = false
-		},
-		FirstLaunch = true,
-		announceLocation = 'SMART',
-		text = '%t Taunted!'
-	}
-	if not SUI.DB.TauntWatcher then
-		SUI.DB.EnabledComponents.TauntWatcher = false
-		SUI.DB.TauntWatcher = Defaults
+	msg = msg:gsub('%%what', target):gsub('%%who', who):gsub('%%spell', GetSpellLink(sid))
+	if failed then
+		msg = msg + ' and it failed horribly.'
+	end
+
+	if ChatChannel == 'SELF' then
+		SUI:Print(msg)
 	else
-		SUI.DB.TauntWatcher = SUI:MergeData(SUI.DB.TauntWatcher, Defaults, false)
+		if not IsInGroup(2) then
+			if IsInRaid() then
+				if ChatChannel == 'INSTANCE_CHAT' then
+					ChatChannel = 'RAID'
+				end
+			elseif IsInGroup(1) then
+				if ChatChannel == 'INSTANCE_CHAT' then
+					ChatChannel = 'PARTY'
+				end
+			end
+		elseif IsInGroup(2) then
+			if ChatChannel == 'RAID' then
+				ChatChannel = 'INSTANCE_CHAT'
+			end
+			if ChatChannel == 'PARTY' then
+				ChatChannel = 'INSTANCE_CHAT'
+			end
+		end
+
+		if ChatChannel == 'SMART' then
+			ChatChannel = 'RAID'
+			if ChatChannel == 'RAID' and not IsInRaid() then
+				ChatChannel = 'PARTY'
+			end
+
+			if ChatChannel == 'PARTY' and not IsInGroup(1) then
+				ChatChannel = 'SAY'
+			end
+
+			if ChatChannel == 'INSTANCE_CHAT' and not IsInGroup(2) then
+				ChatChannel = 'SAY'
+			end
+
+			if ChatChannel == 'CHANNEL' and ec == 0 then
+				ChatChannel = 'SAY'
+			end
+		end
+
+		SendChatMessage(msg, ChatChannel, nil, ec)
 	end
 end
 
+function module:OnInitialize()
+end
+
 function module:COMBAT_LOG_EVENT_UNFILTERED()
-	
+	if not SUI.DB.EnabledComponents.TauntWatcher then
+		return
+	end
+
+	local timeStamp, subEvent, _, _, srcName, _, _, _, dstName, _, _, spellID = CombatLogGetCurrentEventInfo()
+	-- Check if we have been here before
+	if timeStamp == lastTimeStamp and spellID == lastSpellID then
+		return
+	end
+
+	-- Print the taunt
+	if SUI:isInTable(TauntsList, spellID) then
+		local continue = false
+		local inInstance, instanceType = IsInInstance()
+		if instanceType == 'arena' and SUI.DB.TauntWatcher.active.inArena then
+			continue = true
+		elseif inInstance and instanceType == 'party' and SUI.DB.TauntWatcher.active.inParty then
+			continue = true
+		elseif instanceType == 'pvp' and SUI.DB.TauntWatcher.active.inBG then
+			continue = true
+		elseif instanceType == 'raid' and SUI.DB.TauntWatcher.active.inRaid then
+			continue = true
+		elseif (instanceType == 'none' or (not inInstance and instanceType == 'party')) and SUI.DB.TauntWatcher.outdoors then
+			continue = true
+		end
+
+		if not (continue or SUI.DB.TauntWatcher.active.alwayson) then
+			return
+		end
+
+		if subEvent == 'SPELL_AURA_APPLIED' then
+			printFormattedString(srcName, dstName, spellID)
+		elseif subEvent == 'SPELL_MISSED' then
+			printFormattedString(srcName, dstName, spellID, true)
+		else
+			return
+		end
+		-- Update last time and ID
+		lastTimeStamp, lastSpellID = timeStamp, spellID
+	end
+end
+
+function module:OnDisable()
+	module:UnregisterAllEvents()
 end
 
 function module:OnEnable()
 	module:Options()
 
-	TauntWatcher_Watcher:SetScript(
-		'OnEvent',
-		function(_, event)
-			if not SUI.DB.EnabledComponents.TauntWatcher then
-				return
-			end
-
-			if module[event] then
-				module[event]()
-			end
-		end
-	)
-	TauntWatcher_Watcher:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
+	module:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
 end
-
 
 function module:Options()
 	SUI.opt.args['ModSetting'].args['TauntWatcher'] = {
 		type = 'group',
-		name = L['Interrupt announcer'],
+		name = 'Taunt watcher',
 		args = {
 			alwayson = {
 				name = L['Always on'],
@@ -129,7 +213,7 @@ function module:Options()
 					}
 				}
 			},
-			announceLocation = {
+			announceLocation = {3
 				name = 'Announce location',
 				type = 'select',
 				order = 200,
@@ -149,6 +233,56 @@ function module:Options()
 					SUI.DB.TauntWatcher.announceLocation = val
 					RaidFrames:UpdateText()
 				end
+			},
+			TextInfo = {
+				name = '',
+				type = 'group',
+				inline = true,
+				order = 300,
+				args = {
+					a = {
+						name = L['Text variables:'],
+						type = 'description',
+						order = 10,
+						fontSize = 'large'
+					},
+					b = {
+						name = '- %who - ' .. L['Target that was interrupted'],
+						type = 'description',
+						order = 11,
+						fontSize = 'small'
+					},
+					b2 = {
+						name = '- %what - ' .. L['Target that was interrupted'],
+						type = 'description',
+						order = 12,
+						fontSize = 'small'
+					},
+					c = {
+						name = '- %spell - ' .. L['Spell link of spell interrupted'],
+						type = 'description',
+						order = 13,
+						fontSize = 'small'
+					},
+					h = {
+						name = '',
+						type = 'description',
+						order = 499,
+						fontSize = 'medium'
+					},
+					text = {
+						name = L['Announce text:'],
+						type = 'input',
+						order = 501,
+						width = 'full',
+						get = function(info)
+							return SUI.DB.TauntWatcher.text
+						end,
+						set = function(info, value)
+							SUI.DB.TauntWatcher.text = value
+						end
+					}
+				}
 			}
 		}
 	}
