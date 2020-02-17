951c4c88f331c0852bea0646c77ac4a76973f3b5
wutname1 <jeremy12@gmail.com>
2020-02-17 17:31:38 -0600

    Fel Style code reduction

--- Themes/Fel/Style-Setup.lua
+++
@@ -1,6 +0,0 @@
-local SUI = SUI
-----------------------------------------------------------------------------------------------------
---First Lets make the Module
-local module = SUI:NewModule('Style_Fel')
-
-module.Settings = {}
--- Themes/Fel/Style.lua
+++ Themes/Fel/Style.lua
@@ -1,7 +1,42 @@
-local SUI = SUI
-local L = SUI.L
+local SUI, L = SUI, SUI.L
+local print, error = SUI.print, SUI.Error
 local Artwork_Core = SUI:GetModule('Component_Artwork')
-local module = SUI:GetModule('Style_Fel')
+local module = SUI:NewModule('Style_Fel')
+local UnitFrames = SUI:GetModule('Component_UnitFrames')
+local artFrame = CreateFrame('Frame', 'SUI_Art_Fel', SpartanUI)
+module.Settings = {}
+local CurScale
+local petbattle = CreateFrame('Frame')
+local StatusBarSettings = {
+	bars = {
+		'Fel_StatusBar_Left',
+		'Fel_StatusBar_Right'
+	},
+	Fel_StatusBar_Left = {
+		bgImg = 'Interface\\AddOns\\SpartanUI\\Themes\\Classic\\Images\\status-plate-exp',
+		size = {370, 20},
+		TooltipSize = {400, 100},
+		TooltipTextSize = {380, 90},
+		texCords = {0.150390625, 1, 0, 1},
+		GlowPoint = {x = -10},
+		MaxWidth = 32,
+		bgTooltip = 'Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Fel-Box',
+		texCordsTooltip = {0.03125, 0.96875, 0.2578125, 0.7578125}
+	},
+	Fel_StatusBar_Right = {
+		bgImg = 'Interface\\AddOns\\SpartanUI\\Themes\\Classic\\Images\\status-plate-exp',
+		Grow = 'RIGHT',
+		size = {370, 20},
+		TooltipSize = {400, 100},
+		TooltipTextSize = {380, 90},
+		texCords = {0.150390625, 1, 0, 1},
+		GlowPoint = {x = 10},
+		MaxWidth = 35,
+		bgTooltip = 'Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Fel-Box',
+		texCordsTooltip = {0.03125, 0.96875, 0.2578125, 0.7578125}
+	}
+}
+
 ----------------------------------------------------------------------------------------------------
 local InitRan = false
 function module:OnInitialize()
@@ -12,7 +47,8 @@ function module:OnInitialize()
 	SUI.opt.args['General'].args['style'].args['OverallStyle'].args['Digital'].disabled = false
 	--Init if needed
 	if (SUI.DB.Artwork.Style == 'Fel') then
-		module:Init()
+		module:Options()
+		module:InitArtwork()
 	end
 
 	local BarHandler = SUI:GetModule('Component_BarHandler')
@@ -56,30 +92,35 @@ function module:OnInitialize()
 			PVPAlpha = .4
 		}
 	}
-end
 
-function module:Init()
-	module:SetupMenus()
-	module:InitArtwork()
-	InitRan = true
+	module:CreateArtwork()
 end
 
 function module:OnEnable()
 	if (SUI.DB.Artwork.Style ~= 'Fel') then
 		module:Disable()
 	else
-		if (not InitRan) then
-			module:Init()
-		end
 		module:EnableArtwork()
+
+		if SUI.DB.Styles.Fel.SubTheme == 'Digital' then
+			artFrame.Left:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Digital\\Base_Bar_Left')
+			artFrame.Right:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Digital\\Base_Bar_Right')
+			local barBG = 'Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Digital\\Fel-Box'
+			for i = 1, 4 do
+				_G['Fel_Bar' .. i .. 'BG']:SetTexture(barBG)
+			end
+
+			Fel_MenuBarBG:SetTexture(barBG)
+			Fel_StanceBarBG:SetTexture(barBG)
+		end
 	end
 end
 
 function module:OnDisable()
-	SUI_Art_Fel:Hide()
+	artFrame:Hide()
 end
 
-function module:SetupMenus()
+function module:Options()
 	SUI.opt.args['Artwork'].args['Artwork'] = {
 		name = 'Fel Options',
 		type = 'group',
@@ -378,4 +419,169 @@ function module:SetupMenus()
 			}
 		}
 	}
+end
+
+--	Module Calls
+function module:TooltipLoc(_, parent)
+	if (parent == 'UIParent') then
+		tooltip:ClearAllPoints()
+		tooltip:SetPoint('BOTTOMRIGHT', 'SUI_Art_Fel', 'TOPRIGHT', 0, 10)
+	end
+end
+
+function module:BuffLoc(_, parent)
+	BuffFrame:ClearAllPoints()
+	BuffFrame:SetPoint('TOPRIGHT', -13, -13 - (SUI.DB.BuffSettings.offset))
+end
+
+function module:SetupVehicleUI()
+	if SUI.DB.Artwork.VehicleUI then
+		petbattle:HookScript(
+			'OnHide',
+			function()
+				if SUI.DB.EnabledComponents.Minimap and ((SUI.DB.MiniMap.AutoDetectAllowUse) or (SUI.DB.MiniMap.ManualAllowUse)) then
+					Minimap:Hide()
+				end
+				artFrame:Hide()
+			end
+		)
+		petbattle:HookScript(
+			'OnShow',
+			function()
+				if SUI.DB.EnabledComponents.Minimap and ((SUI.DB.MiniMap.AutoDetectAllowUse) or (SUI.DB.MiniMap.ManualAllowUse)) then
+					Minimap:Show()
+				end
+				artFrame:Show()
+			end
+		)
+		RegisterStateDriver(petbattle, 'visibility', '[petbattle] hide; show')
+		RegisterStateDriver(SUI_Art_Fel, 'visibility', '[overridebar][vehicleui] hide; show')
+		RegisterStateDriver(SpartanUI, 'visibility', '[petbattle][overridebar][vehicleui] hide; show')
+	end
+end
+
+function module:RemoveVehicleUI()
+	if SUI.DB.Artwork.VehicleUI then
+		UnregisterStateDriver(petbattle, 'visibility')
+		UnregisterStateDriver(SUI_Art_Fel, 'visibility')
+		UnregisterStateDriver(SpartanUI, 'visibility')
+	end
+end
+
+function module:CreateArtwork()
+	plate = CreateFrame('Frame', 'Fel_ActionBarPlate', SpartanUI, 'Fel_ActionBarsTemplate')
+	plate:SetFrameStrata('BACKGROUND')
+	plate:SetFrameLevel(1)
+	plate:ClearAllPoints()
+	plate:SetAllPoints(SUI_ActionBarAnchor)
+
+	--Setup the Bottom Artwork
+	artFrame:SetFrameStrata('BACKGROUND')
+	artFrame:SetFrameLevel(1)
+	artFrame:SetPoint('BOTTOMLEFT')
+	artFrame:SetPoint('TOPRIGHT', SpartanUI, 'BOTTOMRIGHT', 0, 153)
+
+	artFrame.Left = artFrame:CreateTexture('SUI_Art_War_Left', 'BORDER')
+	artFrame.Left:SetPoint('BOTTOMRIGHT', artFrame, 'BOTTOM', 0, 0)
+	artFrame.Left:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Base_Bar_Left')
+	-- artFrame.Left:SetScale(.75)
+
+	artFrame.Right = artFrame:CreateTexture('SUI_Art_War_Right', 'BORDER')
+	artFrame.Right:SetPoint('BOTTOMLEFT', artFrame, 'BOTTOM')
+	artFrame.Right:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Base_Bar_Right')
+	-- artFrame.Right:SetScale(.75)
+
+	FramerateText:ClearAllPoints()
+	FramerateText:SetPoint('TOPLEFT', SpartanUI, 'TOPLEFT', 10, -10)
+end
+
+function module:EnableArtwork()
+	hooksecurefunc(
+		'UIParent_ManageFramePositions',
+		function()
+			if TutorialFrameAlertButton then
+				TutorialFrameAlertButton:SetParent(Minimap)
+				TutorialFrameAlertButton:ClearAllPoints()
+				TutorialFrameAlertButton:SetPoint('CENTER', Minimap, 'TOP', -2, 30)
+			end
+			CastingBarFrame:ClearAllPoints()
+			CastingBarFrame:SetPoint('BOTTOM', SUI_Art_Fel, 'TOP', 0, 90)
+		end
+	)
+
+	module:SetupVehicleUI()
+
+	if SUI.DB.EnabledComponents.Minimap and ((SUI.DB.MiniMap.AutoDetectAllowUse) or (SUI.DB.MiniMap.ManualAllowUse)) then
+		module:MiniMap()
+	end
+
+	module:StatusBars()
+end
+
+function module:StatusBars()
+	local StatusBars = SUI:GetModule('Artwork_StatusBars')
+	StatusBars:Initalize(StatusBarSettings)
+
+	-- Position the StatusBars
+	StatusBars.bars.Fel_StatusBar_Left:SetPoint('BOTTOMRIGHT', SUI_Art_Fel, 'BOTTOM', -100, 0)
+	StatusBars.bars.Fel_StatusBar_Right:SetPoint('BOTTOMLEFT', SUI_Art_Fel, 'BOTTOM', 100, 0)
+end
+
+-- Minimap
+function module:MiniMapUpdate()
+	if Minimap.BG then
+		Minimap.BG:ClearAllPoints()
+	end
+
+	if SUI.DB.Styles.Fel.SubTheme == 'Digital' then
+		Minimap.BG:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Digital\\Minimap')
+		Minimap.BG:SetPoint('CENTER', Minimap, 'CENTER', 5, -1)
+		Minimap.BG:SetSize(256, 256)
+		Minimap.BG:SetBlendMode('ADD')
+	else
+		if SUI.DB.Styles.Fel.Minimap.Engulfed then
+			Minimap.BG:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Minimap-Engulfed')
+			Minimap.BG:SetPoint('CENTER', Minimap, 'CENTER', 7, 37)
+			Minimap.BG:SetSize(330, 330)
+			Minimap.BG:SetBlendMode('ADD')
+		else
+			Minimap.BG:SetTexture('Interface\\AddOns\\SpartanUI\\Themes\\Fel\\Images\\Minimap-Calmed')
+			Minimap.BG:SetPoint('CENTER', Minimap, 'CENTER', 5, -1)
+			Minimap.BG:SetSize(256, 256)
+			Minimap.BG:SetBlendMode('ADD')
+		end
+	end
+end
+
+function module:MiniMap()
+	if Minimap.ZoneText ~= nil then
+		Minimap.ZoneText:ClearAllPoints()
+		Minimap.ZoneText:SetPoint('TOPLEFT', Minimap, 'BOTTOMLEFT', 0, -5)
+		Minimap.ZoneText:SetPoint('TOPRIGHT', Minimap, 'BOTTOMRIGHT', 0, -5)
+		Minimap.ZoneText:Hide()
+		MinimapZoneText:Show()
+	end
+
+	Minimap.BG = Minimap:CreateTexture(nil, 'BACKGROUND')
+	SUI:GetModule('Component_Minimap'):ShapeChange('circle')
+
+	module:MiniMapUpdate()
+
+	artFrame:HookScript(
+		'OnHide',
+		function(this, event)
+			Minimap:ClearAllPoints()
+			Minimap:SetPoint('TOPRIGHT', UIParent, 'TOPRIGHT', -20, -20)
+			SUI:GetModule('Component_Minimap'):ShapeChange('square')
+		end
+	)
+
+	artFrame:HookScript(
+		'OnShow',
+		function(this, event)
+			Minimap:ClearAllPoints()
+			Minimap:SetPoint('CENTER', SUI_Art_Fel, 'CENTER', 0, 54)
+			SUI:GetModule('Component_Minimap'):ShapeChange('circle')
+		end
+	)
 end
