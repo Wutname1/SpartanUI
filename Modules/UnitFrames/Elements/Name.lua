960a7cdd6f7e401bf0724261fd1b073f051103ad
wutname1 <jeremy12@gmail.com>
2022-05-28 02:51:03 -0500

    Name update function

--- Modules/UnitFrames/Elements/Name.lua
+++ Modules/UnitFrames/Elements/Name.lua
@@ -11,4 +11,13 @@ local function Build(frame, DB)
 	frame:Tag(frame.Name, DB.text)
 end
 
+---@param frame table
+local function Update(frame)
+	local DB = frame.Name.DB
+	SUI:FormatFont(frame.Name, DB.textSize, 'UnitFrames')
+	frame.Name:SetJustifyH(DB.SetJustifyH)
+	frame.Name:SetJustifyV(DB.SetJustifyV)
+	frame:Tag(frame.Name, DB.text)
+end
+
 UF.Elements:Register('Name', Build, Update)
