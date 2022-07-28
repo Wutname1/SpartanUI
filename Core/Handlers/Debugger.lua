47568cfa07040c6e108ed23f241e6951bc10c24d
wutname1 <jeremy12@gmail.com>
2022-07-28 11:29:16 -0500

    debugger update

--- Core/Handlers/Debugger.lua
+++ Core/Handlers/Debugger.lua
@@ -6,38 +6,111 @@ debugger.description = 'Assists with debug information'
 ----------------------------------------------------------------------------------------------------
 local DebugWindow = nil
 local DebugMessages = {}
+local ScrollListing = {}
+local ActiveModule = nil
+
+local function ScrollItemUpdate(_, button, data)
+	button:SetText(data.text)
+	-- checkbox:SetValue(data.value)
+	-- checkbox:SetChecked(true)
+	StdUi:SetObjSize(button, 60, 20)
+	button:SetPoint('RIGHT')
+	button:SetPoint('LEFT')
+	button:HookScript(
+		'OnClick',
+		function(self)
+			ActiveModule = self:GetText()
+			DebugWindow.TextPanel:SetValue('Start of log for ' .. ActiveModule)
+
+			if ActiveModule and DebugMessages[ActiveModule] then
+				for k, v in pairs(DebugMessages[ActiveModule]) do
+					DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n' .. v)
+				end
+			end
+		end
+	)
+	return button
+end
 
 local function CreateDebugWindow()
 	DebugWindow = StdUi:Window(nil, 500, 200, '|cffffffffSpartan|cffe21f1fUI Debug|r info')
 	DebugWindow:SetPoint('TOPLEFT', UIParent, 'TOPLEFT', 5, -5)
-	DebugWindow.TextPanel = StdUi:MultiLineBox(DebugWindow, 480, 200, 'Start of debug...')
+	DebugWindow.TextPanel = StdUi:MultiLineBox(DebugWindow, 480, 200, 'No output active')
 	StdUi:GlueAcross(DebugWindow.TextPanel, DebugWindow, 5, -30, -5, 5)
 	DebugWindow:MakeResizable('BOTTOMRIGHT')
 	DebugWindow:Hide()
+	DebugWindow.Modules = {}
+
+	local OutputSelect = StdUi:Panel(DebugWindow, 180, 200)
+	OutputSelect:SetPoint('TOPLEFT', DebugWindow, 'TOPRIGHT', 5)
+	OutputSelect:SetPoint('BOTTOMLEFT', DebugWindow, 'BOTTOMRIGHT', 5)
+
+	local NamespaceListings = StdUi:FauxScrollFrame(OutputSelect, 160, 180, 15, 20)
+	NamespaceListings:SetPoint('TOP', OutputSelect, 'TOP', 0, -10)
+	NamespaceListings:SetPoint('BOTTOM', OutputSelect, 'BOTTOM', 0, 10)
+
+	table.insert(ScrollListing, {text = 'Core', value = 'core'})
+	for moduleName, _ in pairs(DebugMessages) do
+		table.insert(ScrollListing, {text = (moduleName), value = moduleName})
+	end
+
+	StdUi:ObjectList(
+		NamespaceListings.scrollChild,
+		DebugWindow.Modules,
+		'Button',
+		ScrollItemUpdate,
+		ScrollListing,
+		1,
+		0,
+		0
+	)
+
+	DebugWindow.NamespaceListings = NamespaceListings
+	DebugWindow.OutputSelect = OutputSelect
 end
 
 function SUI.Debug(debugText, module)
-	if not debugger.DB.modules[module] and not debugger.DB.All then
-		debugger.DB.modules[module] = false
-		return
-	end
 	if not DebugMessages[module] then
 		DebugMessages[module] = {}
+		table.insert(ScrollListing, {text = (module), value = module})
+		StdUi:ObjectList(
+			DebugWindow.NamespaceListings.scrollChild,
+			DebugWindow.Modules,
+			'Button',
+			ScrollItemUpdate,
+			ScrollListing,
+			1,
+			0,
+			0
+		)
+		debugger.DB.modules[module] = debugger.DB.enable
+		debugger.options.args[module] = {
+			name = module,
+			type = 'toggle',
+			order = (#debugger.options.args + 1)
+		}
 	end
 
-	table.insert(DebugMessages[module], #DebugMessages[module], tostring(debugText))
-	DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n[' .. module .. '] ' .. tostring(debugText))
+	table.insert(DebugMessages[module], tostring(debugText))
+
+	if ActiveModule and ActiveModule == module then
+		DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n' .. tostring(debugText))
+	end
 end
 
 function debugger:OnInitialize()
 	local defaults = {
-		profile = {
-			enable = false,
-			modules = {}
+		enable = false,
+		modules = {
+			Core = false
 		}
 	}
-	debugger.Database = SUI.SpartanUIDB:RegisterNamespace('Debugger', defaults)
+	debugger.Database = SUI.SpartanUIDB:RegisterNamespace('Debugger', {profile = defaults})
 	debugger.DB = debugger.Database.profile
+
+	for k, _ in pairs(debugger.DB.modules) do
+		DebugMessages[k] = {}
+	end
 end
 
 local function AddOptions()
@@ -73,7 +146,7 @@ local function AddOptions()
 			order = (#options.args + 1)
 		}
 	end
-
+	debugger.options = options
 	SUI.Options:AddOptions(options, nil, 'Help')
 end
 
@@ -88,13 +161,6 @@ function debugger:OnEnable()
 			DebugWindow:Hide()
 		else
 			DebugWindow:Show()
-		end
-		DebugWindow.TextPanel:SetValue('')
-
-		if comp then
-			for k, v in pairs(DebugMessages[comp]) do
-				DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n[' .. comp .. '] ' .. v)
-			end
 		end
 	end
 
