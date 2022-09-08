bbb88446917da16b048fed016e83df97cc44c84c
wutname1 <jeremy12@gmail.com>
2022-09-08 00:54:22 -0500

    fix DF Taint

--- Modules/BarHandlers/WoW.lua
+++ Modules/BarHandlers/WoW.lua
@@ -5,25 +5,20 @@ local module = SUI:GetModule('Component_BarHandler')
 ------------------------------------------------------------
 
 local function OnInitialize(args)
-	local CUSTOM_SYSTEM_ID = 999
-
-	local CustomSystem = CreateFrame('Frame', nil, UIParent)
-	CustomSystem.system = CUSTOM_SYSTEM_ID
-	CustomSystem.systemNameString = 'Custom AddOn System'
-	CustomSystem:EnableMouse(true)
-	CustomSystem:SetClampedToScreen(true)
-
-	CustomSystem:SetPoint('CENTER')
-	CustomSystem:SetSize(400, 32)
-
-	CustomSystem.Selection = CreateFrame('Frame', nil, CustomSystem, 'EditModeSystemSelectionTemplate')
-	CustomSystem.Selection:SetAllPoints(CustomSystem)
-
-	Mixin(CustomSystem, EditModeSystemMixin)
-	CustomSystem:SetScript('OnHide', CustomSystem.OnSystemHide)
-	CustomSystem:OnSystemLoad()
-
-	EditModeSettingDisplayInfoManager.systemSettingDisplayInfo[CUSTOM_SYSTEM_ID] = {}
+	-- local CUSTOM_SYSTEM_ID = 999
+	-- local CustomSystem = CreateFrame('Frame', nil, UIParent)
+	-- CustomSystem.system = CUSTOM_SYSTEM_ID
+	-- CustomSystem.systemNameString = 'Custom AddOn System'
+	-- CustomSystem:EnableMouse(true)
+	-- CustomSystem:SetClampedToScreen(true)
+	-- CustomSystem:SetPoint('CENTER')
+	-- CustomSystem:SetSize(400, 32)
+	-- CustomSystem.Selection = CreateFrame('Frame', nil, CustomSystem, 'EditModeSystemSelectionTemplate')
+	-- CustomSystem.Selection:SetAllPoints(CustomSystem)
+	-- Mixin(CustomSystem, EditModeSystemMixin)
+	-- CustomSystem:SetScript('OnHide', CustomSystem.OnSystemHide)
+	-- CustomSystem:OnSystemLoad()
+	-- EditModeSettingDisplayInfoManager.systemSettingDisplayInfo[CUSTOM_SYSTEM_ID] = {}
 end
 
 local function OnEnable(args)
