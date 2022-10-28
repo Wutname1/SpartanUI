0cff0d48564aa88323d6ad636d95a08a26bc843f
wutname1 <jeremy12@gmail.com>
2022-10-27 22:35:47 -0500

    Lib: StdUi Update

--- libs/StdUi/widgets/Slider.lua
+++ libs/StdUi/widgets/Slider.lua
@@ -1,32 +1,30 @@
----@class StdUi
-local StdUi = LibStub and LibStub('StdUi', true)
+--- @class StdUi
+local StdUi = LibStub and LibStub('StdUi', true);
 if not StdUi then
 	return
 end
 
-local module, version = 'Slider', 7
-if not StdUi:UpgradeNeeded(module, version) then
-	return
-end
+local module, version = 'Slider', 7;
+if not StdUi:UpgradeNeeded(module, version) then return end
 
 ----------------------------------------------------
 --- SliderButton
 ----------------------------------------------------
 
 function StdUi:SliderButton(parent, width, height, direction)
-	local button = self:Button(parent, width, height)
+	local button = self:Button(parent, width, height);
 
-	local texture = self:ArrowTexture(button, direction)
-	texture:SetPoint('CENTER')
+	local texture = self:ArrowTexture(button, direction);
+	texture:SetPoint('CENTER');
 
-	local textureDisabled = self:ArrowTexture(button, direction)
-	textureDisabled:SetPoint('CENTER')
-	textureDisabled:SetDesaturated(0)
+	local textureDisabled = self:ArrowTexture(button, direction);
+	textureDisabled:SetPoint('CENTER');
+	textureDisabled:SetDesaturated(0);
 
-	button:SetNormalTexture(texture)
-	button:SetDisabledTexture(textureDisabled)
+	button:SetNormalTexture(texture);
+	button:SetDisabledTexture(textureDisabled);
 
-	return button
+	return button;
 end
 
 ----------------------------------------------------
@@ -35,50 +33,50 @@ end
 
 --- This is only useful for scrollBars not created using StdUi
 function StdUi:StyleScrollBar(scrollBar)
-	local buttonUp, buttonDown = scrollBar:GetChildren()
+	local buttonUp, buttonDown = scrollBar:GetChildren();
 
-	scrollBar.background = StdUi:Panel(scrollBar)
-	scrollBar.background:SetFrameLevel(scrollBar:GetFrameLevel() - 1)
-	scrollBar.background:SetWidth(scrollBar:GetWidth())
-	self:GlueAcross(scrollBar.background, scrollBar, 0, 1, 0, -1)
+	scrollBar.background = StdUi:Panel(scrollBar);
+	scrollBar.background:SetFrameLevel(scrollBar:GetFrameLevel() - 1);
+	scrollBar.background:SetWidth(scrollBar:GetWidth());
+	self:GlueAcross(scrollBar.background, scrollBar, 0, 1, 0, -1);
 
-	self:StripTextures(buttonUp)
-	self:StripTextures(buttonDown)
+	self:StripTextures(buttonUp);
+	self:StripTextures(buttonDown);
 
-	self:ApplyBackdrop(buttonUp, 'button')
-	self:ApplyBackdrop(buttonDown, 'button')
+	self:ApplyBackdrop(buttonUp, 'button');
+	self:ApplyBackdrop(buttonDown, 'button');
 
-	buttonUp:SetWidth(scrollBar:GetWidth())
-	buttonDown:SetWidth(scrollBar:GetWidth())
+	buttonUp:SetWidth(scrollBar:GetWidth());
+	buttonDown:SetWidth(scrollBar:GetWidth());
 
-	local upTex = self:ArrowTexture(buttonUp, 'UP')
-	upTex:SetPoint('CENTER')
+	local upTex = self:ArrowTexture(buttonUp, 'UP');
+	upTex:SetPoint('CENTER');
 
-	local upTexDisabled = self:ArrowTexture(buttonUp, 'UP')
-	upTexDisabled:SetPoint('CENTER')
-	upTexDisabled:SetDesaturated(0)
+	local upTexDisabled = self:ArrowTexture(buttonUp, 'UP');
+	upTexDisabled:SetPoint('CENTER');
+	upTexDisabled:SetDesaturated(0);
 
-	buttonUp:SetNormalTexture(upTex)
-	buttonUp:SetDisabledTexture(upTexDisabled)
+	buttonUp:SetNormalTexture(upTex);
+	buttonUp:SetDisabledTexture(upTexDisabled);
 
-	local downTex = self:ArrowTexture(buttonDown, 'DOWN')
-	downTex:SetPoint('CENTER')
+	local downTex = self:ArrowTexture(buttonDown, 'DOWN');
+	downTex:SetPoint('CENTER');
 
-	local downTexDisabled = self:ArrowTexture(buttonDown, 'DOWN')
-	downTexDisabled:SetPoint('CENTER')
-	downTexDisabled:SetDesaturated(0)
+	local downTexDisabled = self:ArrowTexture(buttonDown, 'DOWN');
+	downTexDisabled:SetPoint('CENTER');
+	downTexDisabled:SetDesaturated(0);
 
-	buttonDown:SetNormalTexture(downTex)
-	buttonDown:SetDisabledTexture(downTexDisabled)
+	buttonDown:SetNormalTexture(downTex);
+	buttonDown:SetDisabledTexture(downTexDisabled);
 
-	local thumbSize = scrollBar:GetWidth()
-	scrollBar:GetThumbTexture():SetWidth(thumbSize)
+	local thumbSize = scrollBar:GetWidth();
+	scrollBar:GetThumbTexture():SetWidth(thumbSize);
 
-	self:StripTextures(scrollBar)
+	self:StripTextures(scrollBar);
 
-	scrollBar.thumb = self:Panel(scrollBar)
-	scrollBar.thumb:SetAllPoints(scrollBar:GetThumbTexture())
-	self:ApplyBackdrop(scrollBar.thumb, 'button')
+	scrollBar.thumb = self:Panel(scrollBar);
+	scrollBar.thumb:SetAllPoints(scrollBar:GetThumbTexture());
+	self:ApplyBackdrop(scrollBar.thumb, 'button');
 end
 
 ----------------------------------------------------
@@ -87,83 +85,83 @@ end
 
 local SliderMethods = {
 	SetPrecision = function(self, numberOfDecimals)
-		self.precision = numberOfDecimals
+		self.precision = numberOfDecimals;
 	end,
+
 	GetPrecision = function(self)
-		return self.precision
+		return self.precision;
 	end,
+
 	GetValue = function(self)
-		local minimum, maximum = self:GetMinMaxValues()
-		return Clamp(StdUi.Util.roundPrecision(self:OriginalGetValue(), self.precision), minimum, maximum)
+		local minimum, maximum = self:GetMinMaxValues();
+		return Clamp(StdUi.Util.roundPrecision(self:OriginalGetValue(), self.precision), minimum, maximum);
 	end
-}
+};
 
 local SliderEvents = {
 	OnValueChanged = function(self, value, ...)
-		if self.lock then
-			return
-		end
-		self.lock = true
+		if self.lock then return end
+		self.lock = true;
 
-		value = self:GetValue()
+		value = self:GetValue();
 
 		if self.OnValueChanged then
-			self:OnValueChanged(value, ...)
+			self:OnValueChanged(value, ...);
 		end
 
-		self.lock = false
+		self.lock = false;
 	end
 }
 
 function StdUi:Slider(parent, width, height, value, vertical, min, max)
-	local slider = CreateFrame('Slider', nil, parent)
-	self:InitWidget(slider)
-	self:ApplyBackdrop(slider, 'panel')
-	self:SetObjSize(slider, width, height)
+	local slider = CreateFrame('Slider', nil, parent);
+	self:InitWidget(slider);
+	self:ApplyBackdrop(slider, 'panel');
+	self:SetObjSize(slider, width, height);
 
-	slider.vertical = vertical
-	slider.precision = 1
+	slider.vertical = vertical;
+	slider.precision = 1;
 
-	local thumbWidth = vertical and width or 20
-	local thumbHeight = vertical and 20 or height
+	local thumbWidth = vertical and width or 20;
+	local thumbHeight = vertical and 20 or height;
 
-	slider.ThumbTexture = self:Texture(slider, thumbWidth, thumbHeight, self.config.backdrop.texture)
+	slider.ThumbTexture = self:Texture(slider, thumbWidth, thumbHeight, self.config.backdrop.texture);
 	slider.ThumbTexture:SetVertexColor(
 		self.config.backdrop.slider.r,
 		self.config.backdrop.slider.g,
 		self.config.backdrop.slider.b,
 		self.config.backdrop.slider.a
-	)
-	slider:SetThumbTexture(slider.ThumbTexture)
+	);
+	slider:SetThumbTexture(slider.ThumbTexture);
 
-	slider.thumb = self:Frame(slider)
-	slider.thumb:SetAllPoints(slider:GetThumbTexture())
-	self:ApplyBackdrop(slider.thumb, 'button')
+	slider.thumb = self:Frame(slider);
+	slider.thumb:SetAllPoints(slider:GetThumbTexture());
+	self:ApplyBackdrop(slider.thumb, 'button');
 
 	if vertical then
-		slider:SetOrientation('VERTICAL')
-		slider.ThumbTexture:SetPoint('LEFT')
-		slider.ThumbTexture:SetPoint('RIGHT')
+		slider:SetOrientation('VERTICAL');
+		slider.ThumbTexture:SetPoint('LEFT');
+		slider.ThumbTexture:SetPoint('RIGHT');
 	else
-		slider:SetOrientation('HORIZONTAL')
-		slider.ThumbTexture:SetPoint('TOP')
-		slider.ThumbTexture:SetPoint('BOTTOM')
+		slider:SetOrientation('HORIZONTAL');
+		slider.ThumbTexture:SetPoint('TOP');
+		slider.ThumbTexture:SetPoint('BOTTOM');
 	end
 
-	slider.OriginalGetValue = slider.GetValue
+	slider.OriginalGetValue = slider.GetValue;
 
 	for k, v in pairs(SliderMethods) do
-		slider[k] = v
+		slider[k] = v;
 	end
 
-	slider:SetMinMaxValues(min or 0, max or 100)
-	slider:SetValue(value or min or 0)
+	slider:SetMinMaxValues(min or 0, max or 100);
+	slider:SetValue(value or min or 0);
 
 	for k, v in pairs(SliderEvents) do
-		slider:HookScript(k, v)
+		slider:HookScript(k, v);
 	end
 
-	return slider
+	return slider;
 end
 
 ----------------------------------------------------
@@ -172,80 +170,83 @@ end
 
 local SliderWithBoxMethods = {
 	SetValue = function(self, v)
-		self.lock = true
-		self.slider:SetValue(v)
-		v = self.slider:GetValue()
-		self.editBox:SetValue(v)
-		self.value = v
-		self.lock = false
+		self.lock = true;
+		self.slider:SetValue(v);
+		v = self.slider:GetValue();
+		self.editBox:SetValue(v);
+		self.value = v;
+		self.lock = false;
 
 		if self.OnValueChanged then
-			self.OnValueChanged(self, v)
+			self.OnValueChanged(self, v);
 		end
 	end,
-	GetValue = function(self)
-		return self.value
-	end,
-	SetValueStep = function(self, step)
-		self.slider:SetValueStep(step)
-	end,
-	SetPrecision = function(self, numberOfDecimals)
-		self.slider.precision = numberOfDecimals
-	end,
-	GetPrecision = function(self)
-		return self.slider.precision
-	end,
-	SetMinMaxValues = function(self, min, max)
-		self.min = min
-		self.max = max
 
-		self.editBox:SetMinMaxValue(min, max)
-		self.slider:SetMinMaxValues(min, max)
-		self.leftLabel:SetText(min)
-		self.rightLabel:SetText(max)
+	GetValue = function(self)
+		return self.value;
+	end,
+
+	SetValueStep = function(self, step)
+		self.slider:SetValueStep(step);
+	end,
+
+	SetPrecision = function(self, numberOfDecimals)
+		self.slider.precision = numberOfDecimals;
+	end,
+
+	GetPrecision = function(self)
+		return self.slider.precision;
+	end,
+
+	SetMinMaxValues = function(self, min, max)
+		self.min = min;
+		self.max = max;
+
+		self.editBox:SetMinMaxValue(min, max);
+		self.slider:SetMinMaxValues(min, max);
+		self.leftLabel:SetText(min);
+		self.rightLabel:SetText(max);
 	end
-}
+};
 
 local SliderWithBoxOnValueChanged = function(self, val)
-	if self.widget.lock then
-		return
-	end
+	if self.widget.lock then return end;
 
-	self.widget:SetValue(val)
+	self.widget:SetValue(val);
 end
 
 function StdUi:SliderWithBox(parent, width, height, value, min, max)
-	local widget = CreateFrame('Frame', nil, parent)
-	self:SetObjSize(widget, width, height)
+	local widget = CreateFrame('Frame', nil, parent);
+	self:SetObjSize(widget, width, height);
 
-	widget.slider = self:Slider(widget, 100, 12, value, false)
-	widget.editBox = self:NumericBox(widget, 80, 16, value)
-	widget.value = value
-	widget.editBox:SetNumeric(false)
-	widget.leftLabel = self:Label(widget, '')
-	widget.rightLabel = self:Label(widget, '')
+	widget.slider = self:Slider(widget, 100, 12, value, false);
+	widget.editBox = self:NumericBox(widget, 80, 16, value);
+	widget.value = value;
+	widget.editBox:SetNumeric(false);
+	widget.leftLabel = self:Label(widget, '');
+	widget.rightLabel = self:Label(widget, '');
 
-	widget.slider.widget = widget
-	widget.editBox.widget = widget
+	widget.slider.widget = widget;
+	widget.editBox.widget = widget;
 
 	for k, v in pairs(SliderWithBoxMethods) do
-		widget[k] = v
+		widget[k] = v;
 	end
 
 	if min and max then
-		widget:SetMinMaxValues(min, max)
+		widget:SetMinMaxValues(min, max);
 	end
 
-	widget.slider.OnValueChanged = SliderWithBoxOnValueChanged
-	widget.editBox.OnValueChanged = SliderWithBoxOnValueChanged
+	widget.slider.OnValueChanged = SliderWithBoxOnValueChanged;
+	widget.editBox.OnValueChanged = SliderWithBoxOnValueChanged;
 
-	widget.slider:SetPoint('TOPLEFT', widget, 'TOPLEFT', 0, 0)
-	widget.slider:SetPoint('TOPRIGHT', widget, 'TOPRIGHT', 0, 0)
-	self:GlueBelow(widget.editBox, widget.slider, 0, -5, 'CENTER')
-	widget.leftLabel:SetPoint('TOPLEFT', widget.slider, 'BOTTOMLEFT', 0, 0)
-	widget.rightLabel:SetPoint('TOPRIGHT', widget.slider, 'BOTTOMRIGHT', 0, 0)
+	widget.slider:SetPoint('TOPLEFT', widget, 'TOPLEFT', 0, 0);
+	widget.slider:SetPoint('TOPRIGHT', widget, 'TOPRIGHT', 0, 0);
+	self:GlueBelow(widget.editBox, widget.slider, 0, -5, 'CENTER');
+	widget.leftLabel:SetPoint('TOPLEFT', widget.slider, 'BOTTOMLEFT', 0, 0);
+	widget.rightLabel:SetPoint('TOPRIGHT', widget.slider, 'BOTTOMRIGHT', 0, 0);
 
-	return widget
+	return widget;
 end
 
 ----------------------------------------------------
@@ -253,15 +254,15 @@ end
 ----------------------------------------------------
 
 function StdUi:ScrollBar(parent, width, height, horizontal)
-	local panel = self:Panel(parent, width, height)
-	local scrollBar = self:Slider(parent, width, height, 0, not horizontal)
+	local panel = self:Panel(parent, width, height);
+	local scrollBar = self:Slider(parent, width, height, 0, not horizontal);
 
-	scrollBar.ScrollDownButton = self:SliderButton(parent, width, 16, 'DOWN')
-	scrollBar.ScrollUpButton = self:SliderButton(parent, width, 16, 'UP')
-	scrollBar.panel = panel
+	scrollBar.ScrollDownButton = self:SliderButton(parent, width, 16, 'DOWN');
+	scrollBar.ScrollUpButton = self:SliderButton(parent, width, 16, 'UP');
+	scrollBar.panel = panel;
 
-	scrollBar.ScrollUpButton.scrollBar = scrollBar
-	scrollBar.ScrollDownButton.scrollBar = scrollBar
+	scrollBar.ScrollUpButton.scrollBar = scrollBar;
+	scrollBar.ScrollDownButton.scrollBar = scrollBar;
 
 	if horizontal then
 		--@TODO do this
@@ -276,19 +277,19 @@ function StdUi:ScrollBar(parent, width, height, horizontal)
 		--scrollBar:SetPoint('BOTTOMLEFT', scrollBar.ScrollDownButton, 'BOTTOMLEFT', 0, -1);
 		--scrollBar:SetPoint('BOTTOMRIGHT', scrollBar.ScrollDownButton, 'BOTTOMRIGHT', 0, -1);
 	else
-		scrollBar.ScrollUpButton:SetPoint('TOPLEFT', panel, 'TOPLEFT', 0, 0)
-		scrollBar.ScrollUpButton:SetPoint('TOPRIGHT', panel, 'TOPRIGHT', 0, 0)
+		scrollBar.ScrollUpButton:SetPoint('TOPLEFT', panel, 'TOPLEFT', 0, 0);
+		scrollBar.ScrollUpButton:SetPoint('TOPRIGHT', panel, 'TOPRIGHT', 0, 0);
 
-		scrollBar.ScrollDownButton:SetPoint('BOTTOMLEFT', panel, 'BOTTOMLEFT', 0, 0)
-		scrollBar.ScrollDownButton:SetPoint('BOTTOMRIGHT', panel, 'BOTTOMRIGHT', 0, 0)
+		scrollBar.ScrollDownButton:SetPoint('BOTTOMLEFT', panel, 'BOTTOMLEFT', 0, 0);
+		scrollBar.ScrollDownButton:SetPoint('BOTTOMRIGHT', panel, 'BOTTOMRIGHT', 0, 0);
 
-		scrollBar:SetPoint('TOPLEFT', scrollBar.ScrollUpButton, 'BOTTOMLEFT', 0, 1)
-		scrollBar:SetPoint('TOPRIGHT', scrollBar.ScrollUpButton, 'BOTTOMRIGHT', 0, 1)
-		scrollBar:SetPoint('BOTTOMLEFT', scrollBar.ScrollDownButton, 'TOPLEFT', 0, -1)
-		scrollBar:SetPoint('BOTTOMRIGHT', scrollBar.ScrollDownButton, 'TOPRIGHT', 0, -1)
+		scrollBar:SetPoint('TOPLEFT', scrollBar.ScrollUpButton, 'BOTTOMLEFT', 0, 1);
+		scrollBar:SetPoint('TOPRIGHT', scrollBar.ScrollUpButton, 'BOTTOMRIGHT', 0, 1);
+		scrollBar:SetPoint('BOTTOMLEFT', scrollBar.ScrollDownButton, 'TOPLEFT', 0, -1);
+		scrollBar:SetPoint('BOTTOMRIGHT', scrollBar.ScrollDownButton, 'TOPRIGHT', 0, -1);
 	end
 
-	return scrollBar, panel
+	return scrollBar, panel;
 end
 
-StdUi:RegisterModule(module, version)
+StdUi:RegisterModule(module, version);
--- libs/StdUi/widgets/Table.lua
+++ libs/StdUi/widgets/Table.lua
@@ -1,16 +1,16 @@
----@class StdUi
-local StdUi = LibStub and LibStub('StdUi', true)
+--- @class StdUi
+local StdUi = LibStub and LibStub('StdUi', true);
 if not StdUi then
 	return
 end
 
-local module, version = 'Table', 2
+local module, version = 'Table', 2;
 if not StdUi:UpgradeNeeded(module, version) then
 	return
 end
 
-local TableInsert = tinsert
-local StringLength = strlen
+local TableInsert = tinsert;
+local StringLength = strlen;
 
 ----------------------------------------------------
 --- Table
@@ -29,112 +29,117 @@ local StringLength = strlen
 
 local TableMethods = {
 	SetColumns = function(self, columns)
-		self.columns = columns
+		self.columns = columns;
 	end,
+
 	SetData = function(self, data)
-		self.tableData = data
+		self.tableData = data;
 	end,
+
 	AddRow = function(self, row)
 		if not self.tableData then
-			self.tableData = {}
+			self.tableData = {};
 		end
 
-		TableInsert(self.tableData, row)
+		TableInsert(self.tableData, row);
 	end,
+
 	DrawHeaders = function(self)
 		if not self.headers then
-			self.headers = {}
+			self.headers = {};
 		end
 
-		local marginLeft = 0
+		local marginLeft = 0;
 		for i = 1, #self.columns do
-			local col = self.columns[i]
+			local col = self.columns[i];
 
 			if col.header and StringLength(col.header) > 0 then
 				if not self.headers[i] then
 					self.headers[i] = {
-						text = self.stdUi:FontString(self, '')
-					}
+						text = self.stdUi:FontString(self, ''),
+					};
 				end
 
-				local column = self.headers[i]
+				local column = self.headers[i];
 
-				column.text:SetText(col.header)
-				column.text:SetWidth(col.width)
-				column.text:SetHeight(self.rowHeight)
-				column.text:ClearAllPoints()
+				column.text:SetText(col.header);
+				column.text:SetWidth(col.width);
+				column.text:SetHeight(self.rowHeight);
+				column.text:ClearAllPoints();
 				if col.align then
-					column.text:SetJustifyH(col.align)
+					column.text:SetJustifyH(col.align);
 				end
 
-				self.stdUi:GlueTop(column.text, self, marginLeft, 0, 'LEFT')
-				marginLeft = marginLeft + col.width
+				self.stdUi:GlueTop(column.text, self, marginLeft, 0, 'LEFT');
+				marginLeft = marginLeft + col.width;
 
 				column.index = col.index
 				column.width = col.width
 			end
 		end
 	end,
+
 	DrawData = function(self)
 		if not self.rows then
-			self.rows = {}
+			self.rows = {};
 		end
 
-		local marginTop = -self.rowHeight
+		local marginTop = -self.rowHeight;
 		for y = 1, #self.tableData do
-			local row = self.tableData[y]
+			local row = self.tableData[y];
 
-			local marginLeft = 0
+			local marginLeft = 0;
 			for x = 1, #self.columns do
-				local col = self.columns[x]
+				local col = self.columns[x];
 
 				if not self.rows[y] then
-					self.rows[y] = {}
+					self.rows[y] = {};
 				end
 
 				if not self.rows[y][x] then
 					self.rows[y][x] = {
-						text = self.stdUi:FontString(self, '')
-					}
+						text = self.stdUi:FontString(self, '');
+					};
 				end
 
-				local cell = self.rows[y][x]
+				local cell = self.rows[y][x];
 
-				cell.text:SetText(row[col.index])
-				cell.text:SetWidth(col.width)
-				cell.text:SetHeight(self.rowHeight)
-				cell.text:ClearAllPoints()
+				cell.text:SetText(row[col.index]);
+				cell.text:SetWidth(col.width);
+				cell.text:SetHeight(self.rowHeight);
+				cell.text:ClearAllPoints();
 				if col.align then
-					cell.text:SetJustifyH(col.align)
+					cell.text:SetJustifyH(col.align);
 				end
 
-				self.stdUi:GlueTop(cell.text, self, marginLeft, marginTop, 'LEFT')
-				marginLeft = marginLeft + col.width
+				self.stdUi:GlueTop(cell.text, self, marginLeft, marginTop, 'LEFT');
+				marginLeft = marginLeft + col.width;
 			end
 
-			marginTop = marginTop - self.rowHeight
+			marginTop = marginTop - self.rowHeight;
 		end
 	end,
+
 	DrawTable = function(self)
-		self:DrawHeaders()
-		self:DrawData()
+		self:DrawHeaders();
+		self:DrawData();
 	end
-}
+};
 
 function StdUi:Table(parent, width, height, rowHeight, columns, data)
-	local panel = self:Panel(parent, width, height)
-	panel.stdUi = self
-	panel.rowHeight = rowHeight
+	local panel = self:Panel(parent, width, height);
+	panel.stdUi = self;
+	panel.rowHeight = rowHeight;
 
 	for k, v in pairs(TableMethods) do
-		panel[k] = v
+		panel[k] = v;
 	end
 
-	panel:SetColumns(columns)
-	panel:SetData(data)
-	panel:DrawTable()
+	panel:SetColumns(columns);
+	panel:SetData(data);
+	panel:DrawTable();
 
-	return panel
+	return panel;
 end
 
-StdUi:RegisterModule(module, version)
+StdUi:RegisterModule(module, version);
