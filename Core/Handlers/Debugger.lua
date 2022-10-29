0cb128393529fa51e52a2bcdaa2c64781b74542e
wutname1 <jeremy12@gmail.com>
2022-10-29 00:38:32 -0500

    Dev Tool update

--- Core/Handlers/Debugger.lua
+++ Core/Handlers/Debugger.lua
@@ -4,15 +4,13 @@ local StdUi = SUI.StdUi
 local debugger = SUI:NewModule('Handler_Debugger')
 debugger.description = 'Assists with debug information'
 ----------------------------------------------------------------------------------------------------
-local DebugWindow = nil
+local DebugWindow = nil ---@type StdUi.Window
 local DebugMessages = {}
 local ScrollListing = {}
 local ActiveModule = nil
 
 local function ScrollItemUpdate(_, button, data)
 	button:SetText(data.text)
-	-- checkbox:SetValue(data.value)
-	-- checkbox:SetChecked(true)
 	StdUi:SetObjSize(button, 60, 20)
 	button:SetPoint('RIGHT')
 	button:SetPoint('LEFT')
@@ -20,11 +18,11 @@ local function ScrollItemUpdate(_, button, data)
 		'OnClick',
 		function(self)
 			ActiveModule = self:GetText()
-			DebugWindow.TextPanel:SetValue('Start of log for ' .. ActiveModule)
+			DebugWindow.TextPanel:SetText('Start of log for ' .. ActiveModule)
 
 			if ActiveModule and DebugMessages[ActiveModule] then
 				for k, v in pairs(DebugMessages[ActiveModule]) do
-					DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n' .. v)
+					DebugWindow.TextPanel:SetText(DebugWindow.TextPanel:GetText() .. '\n' .. v)
 				end
 			end
 		end
@@ -45,6 +43,15 @@ local function CreateDebugWindow()
 	DebugWindow:Hide()
 	DebugWindow.Modules = {}
 
+	DebugWindow.OpenSettings = StdUi:Button(DebugWindow, 100, 20, 'Open Settings')
+	DebugWindow.OpenSettings:SetPoint('TOPLEFT', DebugWindow, 5, -5)
+	DebugWindow.OpenSettings:SetScript(
+		'OnClick',
+		function()
+			SUI.Options:ToggleOptions({'Help', 'Debug'})
+		end
+	)
+
 	local OutputSelect = StdUi:Panel(DebugWindow, 180, 200)
 	OutputSelect:SetPoint('TOPLEFT', DebugWindow, 'TOPRIGHT', 5)
 	OutputSelect:SetPoint('BOTTOMLEFT', DebugWindow, 'BOTTOMRIGHT', 5)
@@ -53,20 +60,12 @@ local function CreateDebugWindow()
 	NamespaceListings:SetPoint('TOP', OutputSelect, 'TOP', 0, -10)
 	NamespaceListings:SetPoint('BOTTOM', OutputSelect, 'BOTTOM', 0, 10)
 
-	for moduleName, _ in pairs(DebugMessages) do
+	for moduleName, _ in pairs(debugger.DB.modules) do
 		table.insert(ScrollListing, {text = (moduleName), value = moduleName})
 	end
+	DebugWindow.modules = debugger.DB.modules
 
-	StdUi:ObjectList(
-		NamespaceListings.scrollChild,
-		DebugWindow.Modules,
-		'Button',
-		ScrollItemUpdate,
-		ScrollListing,
-		1,
-		0,
-		0
-	)
+	StdUi:ObjectList(NamespaceListings.scrollChild, DebugWindow.Modules, 'Button', ScrollItemUpdate, ScrollListing, 1, 0, 0)
 
 	DebugWindow.NamespaceListings = NamespaceListings
 	DebugWindow.OutputSelect = OutputSelect
@@ -81,16 +80,7 @@ function SUI.Debug(debugText, module)
 	if not DebugMessages[module] then
 		DebugMessages[module] = {}
 		table.insert(ScrollListing, {text = (module), value = module})
-		StdUi:ObjectList(
-			DebugWindow.NamespaceListings.scrollChild,
-			DebugWindow.Modules,
-			'Button',
-			ScrollItemUpdate,
-			ScrollListing,
-			1,
-			0,
-			0
-		)
+		StdUi:ObjectList(DebugWindow.NamespaceListings.scrollChild, DebugWindow.Modules, 'Button', ScrollItemUpdate, ScrollListing, 1, 0, 0)
 		debugger.DB.modules[module] = debugger.DB.enable
 		if debugger.options then
 			debugger.options.args[module] = {
@@ -107,7 +97,7 @@ function SUI.Debug(debugText, module)
 	table.insert(DebugMessages[module], tostring(debugText))
 
 	if ActiveModule and ActiveModule == module then
-		DebugWindow.TextPanel:SetValue(DebugWindow.TextPanel:GetValue() .. '\n' .. tostring(debugText))
+		DebugWindow.TextPanel:SetText(DebugWindow.TextPanel:GetText() .. '\n' .. tostring(debugText))
 	end
 end
 
@@ -121,6 +111,9 @@ local function AddOptions()
 		end,
 		set = function(info, val)
 			debugger.DB.modules[info[#info]] = val
+			if not val and debugger.DB.enable then
+				debugger.DB.enable = false
+			end
 		end,
 		args = {
 			EnableAll = {
@@ -132,6 +125,9 @@ local function AddOptions()
 				end,
 				set = function(info, val)
 					debugger.DB.enable = val
+					for k, _ in pairs(debugger.DB.modules) do
+						debugger.DB.modules[k] = val
+					end
 				end
 			}
 		}
@@ -161,6 +157,10 @@ function debugger:OnInitialize()
 	for k, _ in pairs(debugger.DB.modules) do
 		DebugMessages[k] = {}
 	end
+
+	if SUI:IsModuleEnabled('Component_Chatbox') then
+		debugger:RegisterEvent('ADDON_LOADED')
+	end
 end
 
 function debugger:OnEnable()
@@ -179,4 +179,42 @@ function debugger:OnEnable()
 
 	SUI:AddChatCommand('debug', ToggleDebugWindow, 'Toggles the debug info window display')
 	AddOptions()
+end
+
+local function RefreshData(self)
+	local function OnMouseDown(line, button)
+		local text = line.Text:GetText()
+		if button == 'RightButton' then
+			SUI.Chat:SetEditBoxMessage(text)
+		elseif button == 'MiddleButton' then
+			local rawData = line:GetParent():GetAttributeData().rawValue
+			if rawData.IsObjectType and rawData:IsObjectType('Texture') then
+				_G.TEX = rawData
+				SUI:Print('_G.TEX set to: ', text)
+			else
+				_G.FRAME = rawData
+				SUI:Print('_G.FRAME set to: ', text)
+			end
+		else
+			TableAttributeDisplayValueButton_OnMouseDown(line)
+		end
+	end
+
+	local scrollFrame = self.LinesScrollFrame or TableAttributeDisplay.LinesScrollFrame
+	if not scrollFrame then
+		return
+	end
+	for _, child in next, {scrollFrame.LinesContainer:GetChildren()} do
+		if child.ValueButton and child.ValueButton:GetScript('OnMouseDown') ~= OnMouseDown then
+			child.ValueButton:SetScript('OnMouseDown', OnMouseDown)
+		end
+	end
+end
+
+function debugger:ADDON_LOADED(_, addon)
+	if addon == 'Blizzard_DebugTools' then
+		hooksecurefunc(TableInspectorMixin, 'RefreshAllData', RefreshData)
+		hooksecurefunc(TableAttributeDisplay.dataProviders[2], 'RefreshData', RefreshData)
+		debugger:UnregisterEvent('ADDON_LOADED')
+	end
 end
