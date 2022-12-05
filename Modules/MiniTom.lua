7d2870fa1b21f15af66ca4ffacd0e1d30e4d4084
wutname1 <jeremy12@gmail.com>
2022-12-05 15:51:47 -0600

    MiniTom: Add support for zone names

--- Modules/MiniTom.lua
+++ Modules/MiniTom.lua
@@ -3,9 +3,56 @@ local SUI = SUI
 local module = SUI:NewModule('Module_MiniTom') ---@type SUI.Module
 module.DisplayName = 'MiniTom'
 module.description = 'Enables /way command to set a waypoint on your map'
+local HBD = LibStub('HereBeDragons-2.0')
 
 ---@class MiniTomDB
 local DBDefaults = {}
+
+---Get the MapID for a zone name
+---@param zoneName string
+function module:GetMapID(zoneName)
+	local matches = {}
+
+	---@diagnostic disable-next-line: undefined-field
+	for mapID, mapInfo in pairs(HBD.mapData) do
+		local lname = mapInfo.name:lower()
+		local lzone = zoneName:lower()
+		if lname:match(lzone) then
+			return mapID
+		-- table.insert(matches, {id = mapID, name = mapInfo.name})
+		end
+	end
+
+	if #matches == 1 then
+		return matches[1].id
+	elseif #matches > 1 then
+		SUI:Print('Multiple matches found for zone name')
+		for i = 1, #matches do
+			SUI:Print(matches[i].id)
+			SUI:Print(matches[i].name)
+		end
+	else
+		SUI:Print('No matches found for zone name')
+	end
+end
+
+---Set a waypoint on the map
+---@param mapID number
+---@param x number
+---@param y number
+---@param desc? string
+function module:SetPoint(mapID, x, y, desc)
+	if C_Map.CanSetUserWaypointOnMap(mapID) then
+		local mapPoint = UiMapPoint.CreateFromCoordinates(mapID, tonumber(x) / 100, tonumber(y) / 100, 0)
+		if mapPoint then
+			C_Map.SetUserWaypoint(mapPoint)
+			C_SuperTrack.SetSuperTrackedUserWaypoint(true)
+			SUI:Print('Waypoint set' .. (desc and ' for ' .. desc or ''))
+		end
+	else
+		SUI:Print('Cannot set waypoint on this map')
+	end
+end
 
 local function Options()
 	---@type AceConfigOptionsTable
@@ -43,22 +90,38 @@ function module:OnEnable()
 	end
 
 	function SetWaypoint(args)
-		local x, y = strsplit(' ', args, 2)
-
-		if (x == nil or x == '' or type(tonumber(x)) ~= 'number' or y == nil or y == '' or type(tonumber(y)) ~= 'number') then
-			return
+		-- Remove any commas or periods from the numbers
+		args = args:gsub('(%d)[%.,] (%d)', '%1 %2')
+		local inputSections = {}
+		-- Split the string
+		for item in args:gmatch('%S+') do
+			table.insert(inputSections, item)
 		end
 
-		local mapID = C_Map.GetBestMapForUnit('player')
-		if C_Map.CanSetUserWaypointOnMap(mapID) then
-			local mapPoint = UiMapPoint.CreateFromCoordinates(mapID, tonumber(x) / 100, tonumber(y) / 100, 0)
-			if mapPoint then
-				C_Map.SetUserWaypoint(mapPoint)
-				C_SuperTrack.SetSuperTrackedUserWaypoint(true)
-				SUI:Print('Waypoint set')
+		-- Find the first number and use that to determine the end of the zone name
+		local zoneEnd
+		for i = 1, #inputSections do
+			local token = inputSections[i]
+			if tonumber(token) then
+				zoneEnd = i - 1
+				break
+			end
+		end
+
+		local zone = table.concat(inputSections, ' ', 1, zoneEnd)
+		local x, y, desc = select(zoneEnd + 1, unpack(inputSections))
+
+		if inputSections[1] and not tonumber(inputSections[1]) then
+			-- Find MapID
+			local mapID = module:GetMapID(zone)
+			if mapID then
+				module:SetPoint(mapID, x, y, desc)
+			else
+				SUI:Print('Invalid zone name')
 			end
 		else
-			SUI:Print('Cannot set waypoint on this map')
+			local mapID = C_Map.GetBestMapForUnit('player')
+			module:SetPoint(mapID, x, y, desc)
 		end
 	end
 
