72ab87201692e7c24e08753bd2d8134afc0e0271
wutname1 <jeremy12@gmail.com>
2023-11-07 16:25:22 -0600

    Add ping-receiver attribute to object and frame

--- libs/oUF/oUF.lua
+++ libs/oUF/oUF.lua
@@ -302,6 +302,16 @@ local function initObject(unit, style, styleFunc, header, ...)
 			object:SetAttribute('*type2', 'togglemenu')
 			object:SetAttribute('toggleForVehicle', true)
 
+			--[[ frame.IsPingable
+			This boolean can be set to false to disable the frame from being pingable. Enabled by default.
+			--]]
+			--[[ Override: frame:GetContextualPingType()
+			Used to define which contextual ping is used for the frame.
+
+			By default this wraps `C_Ping.GetContextualPingTypeForUnit(UnitGUID(frame.unit))`.
+			--]]
+			object:SetAttribute('ping-receiver', true)
+
 			if(isEventlessUnit(objectUnit)) then
 				oUF:HandleEventlessUnit(object)
 			else
@@ -345,6 +355,8 @@ local function initObject(unit, style, styleFunc, header, ...)
 		for _, func in next, callback do
 			func(object)
 		end
+
+		Mixin(object, PingableType_UnitFrameMixin)
 
 		-- Make Clique kinda happy
 		if(not object.isNamePlate) then
@@ -594,6 +606,8 @@ do
 				frame:SetAttribute('*type1', 'target')
 				frame:SetAttribute('*type2', 'togglemenu')
 				frame:SetAttribute('oUF-guessUnit', unit)
+
+				frame:SetAttribute('ping-receiver', true)
 			end
 
 			local body = header:GetAttribute('oUF-initialConfigFunction')
