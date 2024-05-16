0dc4260d62f893900b274c06598a47abdb5f1e6e
wutname1 <jeremy12@gmail.com>
2024-05-16 00:54:51 -0500

    Fixes #510 - Update portrait handling logic and element states efficiently.

--- libs/oUF/elements/portrait.lua
+++ libs/oUF/elements/portrait.lua
@@ -40,28 +40,18 @@ local _, ns = ...
 local oUF = ns.oUF
 
 -- ElvUI block
+local UnitIsUnit = UnitIsUnit
 local UnitGUID = UnitGUID
 local UnitIsConnected = UnitIsConnected
 local UnitIsVisible = UnitIsVisible
 local UnitClassBase = UnitClassBase
+local SetPortraitTexture = SetPortraitTexture
 -- end block
 
-local function Update(self, event)
+local function Update(self, event, unit)
+	if not unit or not UnitIsUnit(self.unit, unit) then return end
+
 	local element = self.Portrait
-	if not element then return end
-
-	local unit = self.unit
-	if not unit then return end
-
-	local guid = UnitGUID(unit)
-	local newGUID = element.guid ~= guid
-
-	local nameplate = event == 'NAME_PLATE_UNIT_ADDED'
-	if newGUID then
-		element.guid = guid
-	elseif nameplate then
-		return
-	end
 
 	--[[ Callback: Portrait:PreUpdate(unit)
 	Called before the element has been updated.
@@ -69,30 +59,36 @@ local function Update(self, event)
 	* self - the Portrait element
 	* unit - the unit for which the update has been triggered (string)
 	--]]
-	if(element.PreUpdate) then element:PreUpdate(unit) end
+	if element.PreUpdate then element:PreUpdate(unit) end
 
+	local guid = UnitGUID(unit)
 	local isAvailable = UnitIsConnected(unit) and UnitIsVisible(unit)
-	local hasStateChanged = newGUID or (not nameplate or element.state ~= isAvailable)
+	local hasStateChanged = event ~= 'OnUpdate' or element.guid ~= guid or element.state ~= isAvailable
 	if hasStateChanged then
 		element.playerModel = element:IsObjectType('PlayerModel')
 		element.state = isAvailable
+		element.guid = guid
 
 		if element.playerModel then
 			if not isAvailable then
 				element:SetCamDistanceScale(0.25)
 				element:SetPortraitZoom(0)
 				element:SetPosition(0, 0, 0.25)
+				element:ClearModel()
 				element:SetModel([[Interface\Buttons\TalkToMeQuestionMark.m2]])
 			else
 				element:SetCamDistanceScale(1)
 				element:SetPortraitZoom(1)
 				element:SetPosition(0, 0, 0)
+				element:ClearModel()
 				element:SetUnit(unit)
 			end
-		elseif element.useClassBase then
-			local classBase = UnitClassBase(unit)
-			if classBase then
-				element:SetAtlas('classicon-' .. classBase)
+		elseif not element.customTexture then -- ElvUI changed
+			local class = element.showClass and UnitClassBase(unit)
+			if class then
+				element:SetAtlas('classicon-' .. class)
+			else
+				SetPortraitTexture(element, unit)
 			end
 		end
 	end
@@ -104,9 +100,7 @@ local function Update(self, event)
 	* unit            - the unit for which the update has been triggered (string)
 	* hasStateChanged - indicates whether the state has changed since the last update (boolean)
 	--]]
-	if(element.PostUpdate) then
-		return element:PostUpdate(unit, hasStateChanged)
-	end
+	if element.PostUpdate then return element:PostUpdate(unit, hasStateChanged) end
 end
 
 local function Path(self, ...)
@@ -117,7 +111,7 @@ local function Path(self, ...)
 	* event - the event triggering the update (string)
 	* unit  - the unit accompanying the event (string)
 	--]]
-	return (self.Portrait.Override or Update) (self, ...)
+	return (self.Portrait.Override or Update)(self, ...)
 end
 
 local function ForceUpdate(element)
@@ -126,7 +120,7 @@ end
 
 local function Enable(self, unit)
 	local element = self.Portrait
-	if(element) then
+	if element then
 		element.__owner = self
 		element.ForceUpdate = ForceUpdate
 
@@ -141,9 +135,7 @@ local function Enable(self, unit)
 		--
 		-- DISABLE isn't used as it fires when we most likely don't have the
 		-- information we want.
-		if(unit == 'party') then
-			self:RegisterEvent('PARTY_MEMBER_ENABLE', Path)
-		end
+		if unit == 'party' then self:RegisterEvent('PARTY_MEMBER_ENABLE', Path) end
 
 		element:Show()
 
@@ -153,7 +145,7 @@ end
 
 local function Disable(self)
 	local element = self.Portrait
-	if(element) then
+	if element then
 		element:Hide()
 
 		self:UnregisterEvent('UNIT_MODEL_CHANGED', Path)
