b4ae801a81991387035e3d05e1c2f082ad3aeb34
wutname1 <jeremy12@gmail.com>
2024-08-03 11:08:49 -0500

    StdUI Linting updates

--- libs/StdUi/StdUiGrid.lua
+++ libs/StdUi/StdUiGrid.lua
@@ -1,139 +1,131 @@
---- @class StdUi
-local StdUi = LibStub and LibStub('StdUi', true);
-if not StdUi then
-	return
-end
+---@class StdUi
+local StdUi = LibStub and LibStub('StdUi', true)
+if not StdUi then return end
 
-local module, version = 'Grid', 4;
-if not StdUi:UpgradeNeeded(module, version) then return end;
+local module, version = 'Grid', 4
+if not StdUi:UpgradeNeeded(module, version) then return end
 
 --- Creates frame list that reuses frames and is based on array data
---- @param parent Frame
---- @param create function
---- @param update function
---- @param data table
---- @param padding? number
---- @param oX? number
---- @param oY? number
---- @param limitFn? function
+---@param parent Frame
+---@param create function
+---@param update function
+---@param data table
+---@param padding? number
+---@param oX? number
+---@param oY? number
+---@param limitFn? function
 function StdUi:ObjectList(parent, itemsTable, create, update, data, padding, oX, oY, limitFn)
-	local this = self;
-	oX = oX or 1;
-	oY = oY or -1;
-	padding = padding or 0;
+	local this = self
+	oX = oX or 1
+	oY = oY or -1
+	padding = padding or 0
 
-	if not itemsTable then
-		itemsTable = {};
-	end
+	if not itemsTable then itemsTable = {} end
 
 	for i = 1, #itemsTable do
-		itemsTable[i]:Hide();
+		itemsTable[i]:Hide()
 	end
 
-	local totalHeight = -oY;
+	local totalHeight = -oY
 
-	local i = 1;
+	local i = 1
 	for key, value in pairs(data) do
-		local itemFrame = itemsTable[i];
+		local itemFrame = itemsTable[i]
 
 		if not itemFrame then
 			if type(create) == 'string' then
 				-- create a widget and anchor it to
-				itemsTable[i] = this[create](this, parent);
+				itemsTable[i] = this[create](this, parent)
 			else
-				itemsTable[i] = create(parent, value, i, key);
+				itemsTable[i] = create(parent, value, i, key)
 			end
-			itemFrame = itemsTable[i];
+			itemFrame = itemsTable[i]
 		end
 
 		-- If you create simple widget you need to handle anchoring yourself
-		update(parent, itemFrame, value, i, key);
-		itemFrame:Show();
+		update(parent, itemFrame, value, i, key)
+		itemFrame:Show()
 
-		totalHeight = totalHeight + itemFrame:GetHeight();
+		totalHeight = totalHeight + itemFrame:GetHeight()
 		if i == 1 then
 			-- glue first item to offset
-			this:GlueTop(itemFrame, parent, oX, oY, 'LEFT');
+			this:GlueTop(itemFrame, parent, oX, oY, 'LEFT')
 		else
 			-- glue next items to previous
-			this:GlueBelow(itemFrame, itemsTable[i - 1], 0, -padding);
-			totalHeight = totalHeight + padding;
+			this:GlueBelow(itemFrame, itemsTable[i - 1], 0, -padding)
+			totalHeight = totalHeight + padding
 		end
 
-		if limitFn and limitFn(i, totalHeight, itemFrame:GetHeight()) then
-			break;
-		end
+		if limitFn and limitFn(i, totalHeight, itemFrame:GetHeight()) then break end
 
-		i = i + 1;
+		i = i + 1
 	end
 
-	return itemsTable, totalHeight;
+	return itemsTable, totalHeight
 end
 
 --- Creates frame list that reuses frames and is based on array data
---- @param parent Frame
---- @param create function
---- @param update function
---- @param data table
---- @param paddingX number
---- @param paddingY number
---- @param oX number
---- @param oY number
+---@param parent Frame
+---@param create function
+---@param update function
+---@param data table
+---@param paddingX number
+---@param paddingY number
+---@param oX number
+---@param oY number
 function StdUi:ObjectGrid(parent, itemsMatrix, create, update, data, paddingX, paddingY, oX, oY)
-	oX = oX or 1;
-	oY = oY or -1;
-	paddingX = paddingX or 0;
-	paddingY = paddingY or 0;
+	oX = oX or 1
+	oY = oY or -1
+	paddingX = paddingX or 0
+	paddingY = paddingY or 0
 
-	if not itemsMatrix then
-		itemsMatrix = {};
-	end
+	if not itemsMatrix then itemsMatrix = {} end
 
 	for y = 1, #itemsMatrix do
 		for x = 1, #itemsMatrix[y] do
-			itemsMatrix[y][x]:Hide();
+			itemsMatrix[y][x]:Hide()
 		end
 	end
 
 	for rowI = 1, #data do
-		local row = data[rowI];
+		local row = data[rowI]
 
 		for colI = 1, #row do
 			if not itemsMatrix[rowI] then
 				-- whole row does not exist yet
-				itemsMatrix[rowI] = {};
+				itemsMatrix[rowI] = {}
 			end
 
-			local itemFrame = itemsMatrix[rowI][colI];
+			local itemFrame = itemsMatrix[rowI][colI]
 
 			if not itemFrame then
 				if type(create) == 'string' then
 					-- create a widget and set parent it to
-					itemFrame = self[create](self, parent);
+					itemFrame = self[create](self, parent)
 				else
-					itemFrame = create(parent, data[rowI][colI], rowI, colI);
+					itemFrame = create(parent, data[rowI][colI], rowI, colI)
 				end
-				itemsMatrix[rowI][colI] = itemFrame;
+				itemsMatrix[rowI][colI] = itemFrame
 			end
 
 			-- If you create simple widget you need to handle anchoring yourself
-			update(parent, itemFrame, data[rowI][colI], rowI, colI);
-			itemFrame:Show();
+			update(parent, itemFrame, data[rowI][colI], rowI, colI)
+			itemFrame:Show()
 
 			if rowI == 1 and colI == 1 then
 				-- glue first item to offset
-				self:GlueTop(itemFrame, parent, oX, oY, 'LEFT');
+				self:GlueTop(itemFrame, parent, oX, oY, 'LEFT')
 			else
 				if colI == 1 then
 					-- glue first item in column to previous row
-					self:GlueBelow(itemFrame, itemsMatrix[rowI - 1][colI], 0, -paddingY, 'LEFT');
+					self:GlueBelow(itemFrame, itemsMatrix[rowI - 1][colI], 0, -paddingY, 'LEFT')
 				else
 					-- glue next column to previous column
-					self:GlueRight(itemFrame, itemsMatrix[rowI][colI - 1], paddingX, 0);
+					self:GlueRight(itemFrame, itemsMatrix[rowI][colI - 1], paddingX, 0)
 				end
 			end
 		end
 	end
 end
 
-StdUi:RegisterModule(module, version);
+StdUi:RegisterModule(module, version)
--- libs/StdUi/widgets/Window.lua
+++ libs/StdUi/widgets/Window.lua
@@ -1,74 +1,70 @@
---- @class StdUi
-local StdUi = LibStub and LibStub('StdUi', true);
-if not StdUi then
-	return
-end
+---@class StdUi
+local StdUi = LibStub and LibStub('StdUi', true)
+if not StdUi then return end
 
-local module, version = 'Window', 5;
-if not StdUi:UpgradeNeeded(module, version) then return end;
+local module, version = 'Window', 5
+if not StdUi:UpgradeNeeded(module, version) then return end
 
---- @return StdUi.Window
+---@return StdUi.Window
 function StdUi:Window(parent, width, height, title)
-    parent = parent or UIParent;
+	parent = parent or UIParent
 	---@class StdUi.Window : StdUi.PanelWithTitle
-	local frame = self:PanelWithTitle(parent, width, height, title);
-	frame:SetClampedToScreen(true);
-	frame.titlePanel.isWidget = false;
-	self:MakeDraggable(frame); -- , frame.titlePanel
+	local frame = self:PanelWithTitle(parent, width, height, title)
+	frame:SetClampedToScreen(true)
+	frame.titlePanel.isWidget = false
+	self:MakeDraggable(frame) -- , frame.titlePanel
 
-	local closeBtn = self:Button(frame, 16, 16, 'X');
-	closeBtn.text:SetFontSize(12);
-	closeBtn.isWidget = false;
-	self:GlueTop(closeBtn, frame, -10, -10, 'RIGHT');
+	local closeBtn = self:Button(frame, 16, 16, 'X')
+	closeBtn.text:SetFontSize(12)
+	closeBtn.isWidget = false
+	self:GlueTop(closeBtn, frame, -10, -10, 'RIGHT')
 
 	closeBtn:SetScript('OnClick', function(self)
-		self:GetParent():Hide();
-	end);
+		self:GetParent():Hide()
+	end)
 
-	frame.closeBtn = closeBtn;
+	frame.closeBtn = closeBtn
 
 	function frame:SetWindowTitle(t)
-		self.titlePanel.label:SetText(t);
+		self.titlePanel.label:SetText(t)
 	end
 
 	-- Resizable window shortcut
 	function frame:MakeResizable(direction)
-		StdUi:MakeResizable(frame, direction);
-		return frame;
+		StdUi:MakeResizable(frame, direction)
+		return frame
 	end
 
-	return frame;
+	return frame
 end
 
 -- Reusing dialogs
-StdUi.dialogs = {};
+StdUi.dialogs = {}
 ---@return StdUi.Dialog
 function StdUi:Dialog(title, message, dialogId)
 	---@class StdUi.Dialog : StdUi.Window
-	local window;
+	local window
 	if dialogId and self.dialogs[dialogId] then
-		window = self.dialogs[dialogId];
+		window = self.dialogs[dialogId]
 	else
-		window = self:Window(nil, self.config.dialog.width, self.config.dialog.height, title);
-		window:SetPoint('CENTER');
-		window:SetFrameStrata('DIALOG');
+		window = self:Window(nil, self.config.dialog.width, self.config.dialog.height, title)
+		window:SetPoint('CENTER')
+		window:SetFrameStrata('DIALOG')
 	end
 
 	if window.messageLabel then
-		window.messageLabel:SetText(message);
+		window.messageLabel:SetText(message)
 	else
-		window.messageLabel = self:Label(window, message);
-		window.messageLabel:SetJustifyH('MIDDLE');
-		self:GlueAcross(window.messageLabel, window, 5, -10, -5, 5);
+		window.messageLabel = self:Label(window, message)
+		window.messageLabel:SetJustifyH('MIDDLE')
+		self:GlueAcross(window.messageLabel, window, 5, -10, -5, 5)
 	end
 
-	window:Show();
+	window:Show()
 
-	if dialogId then
-		self.dialogs[dialogId] = window;
-	end
+	if dialogId then self.dialogs[dialogId] = window end
 
-	return window;
+	return window
 end
 
 --- Dialog with additional buttons, buttons can be like this
@@ -82,43 +78,41 @@ end
 ---			onClick = function() end
 ---		}
 --- }
---- @return StdUi.Confirm
+---@return StdUi.Confirm
 function StdUi:Confirm(title, message, buttons, dialogId)
 	---@class StdUi.Confirm : StdUi.Dialog
-	local window = self:Dialog(title, message, dialogId);
+	local window = self:Dialog(title, message, dialogId)
 
 	if buttons and not window.buttons then
-		window.buttons = {};
+		window.buttons = {}
 
-		local btnCount = self.Util.tableCount(buttons);
+		local btnCount = self.Util.tableCount(buttons)
 
-		local btnMargin = self.config.dialog.button.margin;
-		local btnWidth = self.config.dialog.button.width;
-		local btnHeight = self.config.dialog.button.height;
+		local btnMargin = self.config.dialog.button.margin
+		local btnWidth = self.config.dialog.button.width
+		local btnHeight = self.config.dialog.button.height
 
-		local totalWidth = btnCount * btnWidth + (btnCount - 1) * btnMargin;
-		local leftMargin = math.floor((self.config.dialog.width - totalWidth) / 2);
+		local totalWidth = btnCount * btnWidth + (btnCount - 1) * btnMargin
+		local leftMargin = math.floor((self.config.dialog.width - totalWidth) / 2)
 
-		local i = 0;
+		local i = 0
 		for k, btnDefinition in pairs(buttons) do
-			local btn = self:Button(window, btnWidth, btnHeight, btnDefinition.text);
-			btn.window = window;
+			local btn = self:Button(window, btnWidth, btnHeight, btnDefinition.text)
+			btn.window = window
 
-			self:GlueBottom(btn, window, leftMargin + (i * (btnWidth + btnMargin)), 10, 'LEFT');
+			self:GlueBottom(btn, window, leftMargin + (i * (btnWidth + btnMargin)), 10, 'LEFT')
 
-			if btnDefinition.onClick then
-				btn:SetScript('OnClick', btnDefinition.onClick);
-			end
+			if btnDefinition.onClick then btn:SetScript('OnClick', btnDefinition.onClick) end
 
-			window.buttons[k] = btn;
-			i = i + 1;
+			window.buttons[k] = btn
+			i = i + 1
 		end
 
-		window.messageLabel:ClearAllPoints();
-		self:GlueAcross(window.messageLabel, window, 5, -10, -5, 5 + btnHeight + 5);
+		window.messageLabel:ClearAllPoints()
+		self:GlueAcross(window.messageLabel, window, 5, -10, -5, 5 + btnHeight + 5)
 	end
 
-	return window;
+	return window
 end
 
-StdUi:RegisterModule(module, version);
+StdUi:RegisterModule(module, version)
