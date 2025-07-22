af2e84a6f4c9537fda666abc123dd6e108a858ec
wutname1 <jeremy12@gmail.com>
2025-07-22 01:51:21 -0500

    AuraBars updates

--- Modules/UnitFrames/Elements/AuraBars.lua
+++ Modules/UnitFrames/Elements/AuraBars.lua
@@ -1,22 +1,146 @@
 local UF = SUI.UF
 local L = SUI.L
--- local AuraFilter = UF.AuraFilter
 
--- function UF:AuraBars_UpdateBar(bar)
--- 	local bars = bar:GetParent()
--- 	bar.db = bars.db
+-- Healing over Time spell lists for easy healer filtering
+local HealingSpells = {
+	-- Druid HoTs
+	[774] = true, -- Rejuvenation
+	[8936] = true, -- Regrowth
+	[33763] = true, -- Lifebloom
+	[48438] = true, -- Wild Growth
+	[102351] = true, -- Cenarion Ward
+	[102342] = true, -- Ironbark
+	[200389] = true, -- Cultivation
+	[157982] = true, -- Tranquility
+	[391891] = true, -- Adaptive Swarm (Healing)
+	[383193] = true, -- Grove Tending
+	[200851] = true, -- Rage of the Sleeper
 
--- 	bar:SetReverseFill(bars.reverseFill)
--- 	bar.spark:ClearAllPoints()
--- 	bar.spark:Point(bars.reverseFill and 'LEFT' or 'RIGHT', bar:GetStatusBarTexture())
--- 	bar.spark:Point('BOTTOM')
--- 	bar.spark:Point('TOP')
+	-- Priest HoTs
+	[139] = true, -- Renew
+	[17] = true, -- Power Word: Shield
+	[194384] = true, -- Atonement
+	[41635] = true, -- Prayer of Mending
+	[33206] = true, -- Pain Suppression
+	[47753] = true, -- Divine Aegis
+	[10060] = true, -- Power Infusion
+	[265202] = true, -- Holy Word: Salvation
+	[372835] = true, -- Lightwell Renew
+	[200183] = true, -- Apotheosis
 
--- 	AuraBars.spellTimeFont = SUI.Font:GetFontFace('Player')
--- 	AuraBars.spellNameFont = SUI.Font:GetFontFace('Player')
--- 	UF:Update_FontString(bar.timeText)
--- 	UF:Update_FontString(bar.nameText)
--- end
+	-- Shaman HoTs
+	[61295] = true, -- Riptide
+	[974] = true, -- Earth Shield
+	[16237] = true, -- Ancestral Fortitude
+	[201633] = true, -- Earthen Wall Totem
+	[383648] = true, -- Flame Shock (Enhancement healing)
+	[462844] = true, -- Surging Totem
+	[108271] = true, -- Astral Shift
+
+	-- Paladin HoTs/Buffs
+	[53563] = true, -- Beacon of Light
+	[200025] = true, -- Beacon of Virtue
+	[156910] = true, -- Beacon of Faith
+	[1022] = true, -- Blessing of Protection
+	[6940] = true, -- Blessing of Sacrifice
+	[1044] = true, -- Blessing of Freedom
+	[305395] = true, -- Blessing of Sanctuary
+	[223306] = true, -- Bestow Faith
+	[148039] = true, -- Barrier of Faith
+	[200654] = true, -- Tyr's Deliverance
+
+	-- Monk HoTs
+	[191840] = true, -- Essence Font
+	[124682] = true, -- Enveloping Mist
+	[115175] = true, -- Soothing Mist
+	[116849] = true, -- Life Cocoon
+	[325209] = true, -- Enveloping Breath
+	[388193] = true, -- Faeline Stomp
+	[343737] = true, -- Refreshing Jade Wind
+	[116844] = true, -- Ring of Peace
+	[122783] = true, -- Diffuse Magic
+
+	-- Evoker HoTs
+	[355941] = true, -- Dream Breath
+	[364343] = true, -- Echo
+	[367230] = true, -- Reversion
+	[376788] = true, -- Dream Flight
+	[363534] = true, -- Rewind
+	[374348] = true, -- Renewing Blaze
+	[378441] = true, -- Time Stop
+	[370960] = true, -- Emerald Communion
+	[374227] = true, -- Zephyr
+
+	-- Demon Hunter
+	[203819] = true, -- Demon Spikes
+	[212800] = true, -- Blur
+	[263648] = true, -- Soul Barrier
+	[187827] = true, -- Metamorphosis
+
+	-- Death Knight
+	[48707] = true, -- Anti-Magic Shell
+	[55233] = true, -- Vampiric Blood
+	[194679] = true, -- Rune Tap
+	[81256] = true, -- Dancing Rune Weapon
+	[219809] = true, -- Tombstone
+
+	-- Warrior
+	[871] = true, -- Shield Wall
+	[12975] = true, -- Last Stand
+	[184364] = true, -- Enraged Regeneration
+	[97462] = true, -- Rallying Cry
+	[223658] = true, -- Safeguard
+
+	-- General defensive/healing buffs
+	[1459] = true, -- Arcane Intellect
+	[21562] = true, -- Power Word: Fortitude
+	[6673] = true, -- Battle Shout
+	[1126] = true, -- Mark of the Wild
+}
+
+-- DoT spells for DPS tracking
+local DamageOverTimeSpells = {
+	-- Warlock
+	[172] = true, -- Corruption
+	[980] = true, -- Agony
+	[27243] = true, -- Seed of Corruption
+	[348] = true, -- Immolate
+	[157736] = true, -- Immolate (Destro)
+	[30108] = true, -- Unstable Affliction
+	[63106] = true, -- Siphon Soul
+	[234153] = true, -- Drain Life
+	[198590] = true, -- Drain Soul
+	[5138] = true, -- Drain Mana
+
+	-- Shadow Priest
+	[589] = true, -- Shadow Word: Pain
+	[34914] = true, -- Vampiric Touch
+	[15407] = true, -- Mind Flay
+	[48045] = true, -- Mind Sear
+	[8122] = true, -- Psychic Scream
+
+	-- Rogue
+	[1943] = true, -- Rupture
+	[2818] = true, -- Deadly Poison
+	[8680] = true, -- Wound Poison
+	[3408] = true, -- Crippling Poison
+	[121411] = true, -- Crimson Tempest
+	[122233] = true, -- Crimson Poison
+
+	-- Hunter
+	[1978] = true, -- Serpent Sting
+	[3674] = true, -- Black Arrow
+	[13795] = true, -- Immolation Trap
+	[271788] = true, -- Serpent Sting (BM)
+	[259491] = true, -- Serpent Sting (Survival)
+
+	-- Mage
+	[12654] = true, -- Ignite
+	[22959] = true, -- Fire Vulnerability
+	[31661] = true, -- Dragon's Breath
+	[413841] = true, -- Frostfire Bolt
+	[205708] = true, -- Chilled to the Bone
+}
 
 ---@param frame table
 ---@param DB table
@@ -32,9 +156,49 @@ local function Build(frame, DB)
 	---@param unit UnitId
 	---@param data UnitAuraInfo
 	local FilterAura = function(element, unit, data)
-		return UF.Auras:Filter(element, unit, data, element.DB.rules)
+		-- Use enhanced filtering system
+		return UF.Auras:Filter(element, unit, data, element.DB.rules) and element:CustomAuraFilter(unit, data)
 	end
 	element.FilterAura = FilterAura
+
+	-- Enhanced custom filter function for AuraBars
+	---@param unit UnitId
+	---@param data UnitAuraInfo
+	function element:CustomAuraFilter(unit, data)
+		local DB = self.DB
+
+		-- If using legacy custom filter, fall back to that
+		if DB.useLegacyFilter then
+			if (data.sourceUnit == 'player' or data.sourceUnit == 'vehicle' or data.isBossAura) and data.duration ~= 0 and data.duration <= 900 then return true end
+			return false
+		end
+
+		-- Raider mode: always show boss auras regardless of role
+		if DB.raiderMode and data.isBossAura then return true end
+
+		-- Enhanced filtering with role presets
+		if DB.filterMode == 'healer' then
+			-- Healer mode: ONLY show HoTs and defensive buffs in the list
+			if HealingSpells[data.spellId] then return true end
+			-- Also show boss auras for healers
+			if data.isBossAura then return true end
+		elseif DB.filterMode == 'dps' then
+			-- DPS mode: ONLY show DoTs and offensive buffs in the list
+			if DamageOverTimeSpells[data.spellId] and data.sourceUnit == 'player' then return true end
+			-- Also show boss auras for DPS
+			if data.isBossAura then return true end
+		elseif DB.filterMode == 'tank' then
+			-- Tank mode: show defensive buffs and important short debuffs
+			if data.sourceUnit == 'player' and (HealingSpells[data.spellId] or data.duration <= 60) then return true end
+			-- Also show boss auras for tanks
+			if data.isBossAura then return true end
+		elseif DB.filterMode == 'custom' then
+			-- Custom mode: use fallback filtering rules
+			if data.isBossAura or (data.sourceUnit == 'player' and data.duration > 0 and data.duration <= DB.maxDuration) then return true end
+		end
+
+		return false
+	end
 
 	local function PostCreateBar(_, bar)
 		bar:SetStatusBarTexture(UF:FindStatusBarTexture(DB.texture))
@@ -51,12 +215,23 @@ local function Build(frame, DB)
 	end
 	element.PostCreateBar = PostCreateBar
 
+	-- Legacy CustomFilter for compatibility
 	---@param element any
 	---@param unit any
 	---@param bar any
 	---@param auraData AuraData
 	element.CustomFilter = function(element, unit, bar, auraData)
-		if (auraData.sourceUnit == 'player' or auraData.sourceUnit == 'vehicle' or auraData.isBossAura) and auraData.duration ~= 0 and auraData.duration <= 900 then return true end
+		-- Convert bar data to standard format for new filter
+		local data = {
+			spellId = auraData.spellId,
+			sourceUnit = auraData.sourceUnit,
+			isBossAura = auraData.isBossAura,
+			duration = auraData.duration,
+			name = auraData.name,
+			isHelpful = not auraData.isHarmful,
+			isHarmful = auraData.isHarmful,
+		}
+		return element:CustomAuraFilter(unit, data)
 	end
 
 	element.displayReasons = {}
@@ -98,6 +273,128 @@ local function Options(unitName, OptionSet)
 		UF.DB.UserSettings[UF.DB.Style][unitName].elements.AuraBars[option] = val
 		UF.Unit[unitName]:ElementUpdate('AuraBars')
 	end
+
+	-- Add Filter Mode options
+	OptionSet.args.FilterMode = {
+		name = L['Filter Mode'],
+		type = 'group',
+		order = 50,
+		inline = true,
+		args = {
+			filterMode = {
+				name = L['Filtering Mode'],
+				desc = L['Choose how aura bars are filtered. Healer mode shows HoTs, DPS mode shows DoTs, Tank mode shows defensive buffs.'],
+				type = 'select',
+				order = 1,
+				values = {
+					healer = L['Healer (HoTs & Defensive)'],
+					dps = L['DPS (DoTs & Offensive)'],
+					tank = L['Tank (Defensive & Short Buffs)'],
+					custom = L['Custom (Use Advanced Filters)'],
+				},
+				get = function()
+					return ElementSettings.filterMode
+				end,
+				set = function(_, val)
+					OptUpdate('filterMode', val)
+				end,
+			},
+			raiderMode = {
+				name = L['Raider Mode'],
+				desc = L['Always show all boss buffs and debuffs regardless of role preset'],
+				type = 'toggle',
+				order = 2,
+				get = function()
+					return ElementSettings.raiderMode
+				end,
+				set = function(_, val)
+					OptUpdate('raiderMode', val)
+				end,
+			},
+			useLegacyFilter = {
+				name = L['Use Legacy Filtering'],
+				desc = L['Use the original filtering system (player/vehicle/boss auras under 15 minutes) instead of role-based filtering'],
+				type = 'toggle',
+				order = 3,
+				get = function()
+					return ElementSettings.useLegacyFilter
+				end,
+				set = function(_, val)
+					OptUpdate('useLegacyFilter', val)
+				end,
+			},
+			maxDuration = {
+				name = L['Maximum Duration'],
+				desc = L['Maximum duration in seconds for player auras to be shown (when not using role presets)'],
+				type = 'range',
+				order = 3,
+				min = 30,
+				max = 3600,
+				step = 30,
+				get = function()
+					return ElementSettings.maxDuration
+				end,
+				set = function(_, val)
+					OptUpdate('maxDuration', val)
+				end,
+			},
+		},
+	}
+
+	-- Add standard filtering options using the shared system
+	local FilterGet = function(info, key)
+		if info[#info - 1] == 'duration' then
+			return ElementSettings.rules.duration[info[#info]] or false
+		else
+			return ElementSettings.rules[key] or false
+		end
+	end
+
+	local FilterSet = function(info, key, val)
+		if info[#info - 1] == 'duration' then
+			if (info[#info] == 'minTime') and key > ElementSettings.rules.duration.maxTime then
+				return
+			elseif (info[#info] == 'maxTime') and key < ElementSettings.rules.duration.minTime then
+				return
+			end
+			UF.CurrentSettings[unitName].elements.AuraBars.rules.duration[info[#info]] = key
+			UF.DB.UserSettings[UF.DB.Style][unitName].elements.AuraBars.rules.duration[info[#info]] = key
+		else
+			UF.CurrentSettings[unitName].elements.AuraBars.rules[info[#info]] = key
+			UF.DB.UserSettings[UF.DB.Style][unitName].elements.AuraBars.rules[info[#info]] = key
+		end
+		UF.Unit[unitName]:ElementUpdate('AuraBars')
+	end
+
+	UF.Options:AddAuraFilters(unitName, OptionSet, FilterSet, FilterGet)
+
+	-- Add whitelist/blacklist options
+	local additem = function(info, input)
+		local spellId
+		if type(input) == 'string' then
+			-- See if we got a spell link
+			if input:find('|Hspell:%d+') then
+				spellId = tonumber(input:match('|Hspell:(%d+)'))
+			elseif input:find('%[(.-)%]') then
+				local spellInfo = C_Spell.GetSpellInfo(input:match('%[(.-)%]'))
+				spellId = spellInfo and spellInfo.spellID
+			else
+				local spellInfo = C_Spell.GetSpellInfo(input)
+				spellId = spellInfo and spellInfo.spellID
+			end
+			if not spellId then
+				SUI:Print('Invalid spell name or ID')
+				return
+			end
+		end
+
+		ElementSettings.rules[info[#info - 1]][spellId] = true
+		UF.DB.UserSettings[UF.DB.Style][unitName].elements.AuraBars.rules[info[#info - 1]][spellId] = true
+
+		UF.Unit[unitName]:ElementUpdate('AuraBars')
+	end
+
+	UF.Options:AddAuraWhitelistBlacklist(unitName, OptionSet, additem)
 
 	OptionSet.args.Layout = {
 		name = L['Layout'],
@@ -240,6 +537,11 @@ local Settings = {
 	gap = 1,
 	scaleTime = false,
 	icon = true,
+	-- Enhanced filtering options
+	filterMode = 'custom', -- 'healer', 'dps', 'tank', 'custom'
+	raiderMode = false,
+	useLegacyFilter = true,
+	maxDuration = 900, -- 15 minutes in seconds
 	position = {
 		anchor = 'BOTTOMLEFT',
 		relativePoint = 'TOPLEFT',
@@ -247,7 +549,16 @@ local Settings = {
 		y = 20,
 	},
 	rules = {
+		duration = {
+			enabled = false,
+			mode = 'exclude',
+			maxTime = 900,
+			minTime = 1,
+		},
 		showPlayers = true,
+		isBossAura = true,
+		whitelist = {},
+		blacklist = {},
 	},
 	config = {
 		type = 'Auras',
