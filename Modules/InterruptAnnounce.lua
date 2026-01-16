377181685305d860c93b98ff1a436a47463cd106
wutname1 <jeremy12@gmail.com>
2026-01-15 22:52:20 -0600

    update depreciated calls

--- Modules/InterruptAnnounce.lua
+++ Modules/InterruptAnnounce.lua
@@ -20,20 +20,20 @@ local function printFormattedString(t, sid, spell, ss, ssid, inputstring)
 			elseif IsInGroup(1) then
 				ChatChannel = 'PARTY'
 			end
-			if ChatChannel then SendChatMessage(msg, ChatChannel) end
+			if ChatChannel then C_ChatInfo.SendChatMessage(msg, ChatChannel) end
 		else
 			if DBChannel == 'RAID' or DBChannel == 'INSTANCE_CHAT' then
 				if IsInRaid() and IsInGroup(2) then
 					-- We are in a raid with instance chat
-					SendChatMessage(msg, 'INSTANCE_CHAT')
+					C_ChatInfo.SendChatMessage(msg, 'INSTANCE_CHAT')
 					return
 				elseif IsInRaid() and not IsInGroup(2) then
 					-- We are in a manual Raid
-					SendChatMessage(msg, 'RAID')
+					C_ChatInfo.SendChatMessage(msg, 'RAID')
 					return
 				end
 			elseif DBChannel == 'PARTY' and IsInGroup(1) then
-				SendChatMessage(msg, 'PARTY')
+				C_ChatInfo.SendChatMessage(msg, 'PARTY')
 				return
 			end
 		end
